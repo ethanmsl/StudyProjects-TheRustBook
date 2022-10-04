b979b956929bae90953050a1011a3705373befa6
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-04 06:37:16 -0700

    more loops and fib_nums

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -45,4 +45,57 @@ fn main() {
         count += 1;
     }
     println!("End count = {count}");
+
+    // while loop syntax
+    let mut number = 3;
+    while number != 0 {
+        println!("{number}");
+        number -= 1;
+    }
+    println!("countdown while loop exited");
+    //println!("can I embed a number and have it print? zero: {0}");
+    // ^ No, I could not. (got commentary on invalid ref to positional arg)
+
+    // for loop syntax
+    let arr = [10, 20, 30, 40, 50];
+    // ^ appears that arr's type is compiler inferred
+    // ('[i32;5]', with 'i32' breaking symmetry by way of preference)
+    for element in arr {
+        println!("the value: {element}");
+    }
+
+    // for loop countdown (range syntax)
+    for number in 1..=3 {
+        println!("for-loop countdown: {number}")
+    }
+    println!("for-loop countdown exited");
+
+    println!("-------------------------");
+    for number in 1..=10 {
+        let fib_num = fibonacci_nth(number);
+        let postfix = match number {
+            1 => "st",
+            2 => "nd",
+            3 => "rd",
+            _ => "th",
+        };
+        println!("The {number}{postfix} Fibonacci number is {fib_num}");
+    }
+}
+
+
+fn fibonacci_nth(n: u32) ->u32 {
+    let mut prev = 0;
+    let mut current = 1;
+    let mut temp_value;
+
+    for _ in 1..=n {
+        // new val
+        temp_value = prev + current;
+
+        // shift values
+        prev = current;
+        current = temp_value;
+    }
+    current
 }
