b38f05cf4ee850853c7ed6ca77e104854a6314a8
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-06 12:38:48 -0700

    some nested enums / state quarters

--- enum_match_06/src/main.rs
+++ enum_match_06/src/main.rs
@@ -1,13 +1,24 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    California,
+    Texas,
+    Vermont,
+}
 enum Coin {
     Penny,
     Nickle,
     Dime,
-    Quarter,
+    Quarter(UsState),
+    // ^ Quarter along with info about which state is on it
 }
 
 fn main() {
     let nickle_coin = Coin::Nickle;
     println!("Value of a Nickle: {}", value_in_cents(nickle_coin));
+    let texas_quarter = Coin::Quarter(UsState::Texas);
+    println!("Value of a Quarter: {}", value_in_cents(texas_quarter));
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -18,6 +29,10 @@ fn value_in_cents(coin: Coin) -> u8 {
         },
         Coin::Nickle => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("Ooh, a State quarter from {:?}.", state);
+            25
+        }
+        //Coin::Quarter(_) => 25,
     }
 }
