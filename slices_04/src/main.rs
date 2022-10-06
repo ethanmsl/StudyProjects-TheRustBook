e187707074b9b76c2f54bc60d61a55337847050d
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-05 22:00:51 -0700

    some slicing and a without slicing attempt

--- slices_04/src/main.rs
+++ slices_04/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello world");
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    println!("world = {world}");
+    println!("hello = {hello}");
+}
+
+
+// example of trying to work with data minus slices
+// using memory-independent, but conceptually-depenedent values
+fn _first_word(s: &String) -> usize /*index of word*/ {
+    let bytes = s.as_bytes();
+    
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i; // if we find a ' '
+        }
+    }
+
+    s.len() // if we don't find a ' '
 }
