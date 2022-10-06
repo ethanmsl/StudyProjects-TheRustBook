7f084609c73943f12b892afd5ce03c5e84365dfc
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-06 02:23:03 -0700

    struct and instantiation

--- structs_05/src/main.rs
+++ structs_05/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    // instantiating an instance of struct
+    // NOTE: order of field definition doesn't matter
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+}
+
+
+// defining struct
+struct User{
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
