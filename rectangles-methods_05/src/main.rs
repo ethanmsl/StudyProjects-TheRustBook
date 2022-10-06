651769053d69626f9809ad230841b3cfa3ca9968
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-06 08:33:55 -0700

    associatted function square(<size>)

--- rectangles-methods_05/src/main.rs
+++ rectangles-methods_05/src/main.rs
@@ -6,12 +6,26 @@ struct Rectangle {
 
 // 'impl': "implementation"
 impl Rectangle {
+    // method with no explicit arguments
+    // <rect_instance>.area()
     fn area(&self) -> u32 {
         self.width * self.height
     }
 
+    // takes and additional argument
+    // <rect_instance>.can_hold(<another_rect_instance>)
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
+    }
+
+    // non-method Associatted Function
+    // Rectangle::square(<size>)
+    // ^ note that this takes the type itselt, not an instance
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
     }
 }
 
@@ -26,10 +40,7 @@ fn main() {
         rect1.area()
     );
 
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
+    let rect2 = Rectangle::square(12);
     let rect3 = Rectangle {
         width: 60,
         height: 45,
