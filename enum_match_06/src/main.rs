9ad856a4b45c7e1faa591c33675e243410256b02
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-06 12:29:47 -0700

    coin conversion

--- enum_match_06/src/main.rs
+++ enum_match_06/src/main.rs
@@ -1,3 +1,23 @@
+enum Coin {
+    Penny,
+    Nickle,
+    Dime,
+    Quarter,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let nickle_coin = Coin::Nickle;
+    println!("Value of a Nickle: {}", value_in_cents(nickle_coin));
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickle => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
 }
