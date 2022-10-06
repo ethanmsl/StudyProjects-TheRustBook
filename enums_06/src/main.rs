c06420ae7d7080e88a6a2a3d80930b9ca2d184d9
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-06 09:15:11 -0700

    enums and struct-enum combos

--- enums_06/src/main.rs
+++ enums_06/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+// context:  there are two main IP address standards
+enum IpAddrKind {
+    V4,
+    V6,
 }
+
+enum IpAddr {
+    V4(String),
+    V6(String),
+}
+
+// Struct with an enum valued field
+struct IpAddr_struct {
+    kind: IpAddrKind,
+    address: String,
+}
+
+
+
+fn main() {
+    let _four = IpAddrKind::V4;
+    let _six = IpAddrKind::V6;
+
+    // route takes either enum of 'IpAddrKind'
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
+    //route(IpAddrKind);
+    // ^ but this generates an error
+    //   even though route takes 'IpAddrKind', it, speifically, seems to
+    //   take an enum value, not the whole thing
+
+    // Clunky Non-Exemplar
+    // two instances of 'IpAddr' struct
+    let _home_structured = IpAddr_struct {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+    let _loopback_structured = IpAddr_struct {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    // nicer example
+    // using an enum with... subtypes(?)
+    let home = IpAddr::V4(String::from("127.0.0.1"))
+    let loopback = IpAddr::V6(String::from("::1"))
+}
+
+fn route(IpAddrKind: IpAddrKind) {}
