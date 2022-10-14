80e01ab7cc74b7a4749819a33526aa9fcab7414f
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-13 23:01:58 -0700

    paths and commenting out init code

--- restaurant-lib_07/src/lib.rs
+++ restaurant-lib_07/src/lib.rs
@@ -1,17 +1,19 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// // included code from 'cargo new <...> --lib'
+// // like the 'hello world' code that plain 'cargo new <...>' creates
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
 
 
 mod front_of_house {
@@ -28,4 +30,13 @@ mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+
+pub fn eat_at_restauraunt() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
