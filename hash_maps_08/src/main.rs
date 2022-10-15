2aea58c82f256f954966dffd217cea918069d77a
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 11:35:17 -0700

    make hashmap instnace and insert some pairs

--- hash_maps_08/src/main.rs
+++ hash_maps_08/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    use std::collections::HashMap;
+    let mut scores = HashMap::new();
+
+    // NOTE: no std method for constructing hashes
+    //       unlike strings and vectors e.g. string::from("...") & vec!([...])
+
+    // using .insert(k,v) to add mappings to the hashmap
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
 }
