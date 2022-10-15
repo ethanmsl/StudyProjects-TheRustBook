d48ec84efa818fae543a5a7fbca6cf20d36ad55e
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 00:32:39 -0700

    add (+) and format! for strings

--- strings_08/src/main.rs
+++ strings_08/src/main.rs
@@ -31,8 +31,23 @@ fn main() {
 
     let s1 = String::from("Hello, ");
     let s2 = String::from("world!");
-    let s3 = s1 + &s2;
-    // ^ note s1 (but not s2) has been moved here and can no longer be used
+    let s3 = s1 + &s2; // ^ note s1 (but not s2) has been moved here and can no longer be used
     println!("s3 is {s3}, s2 is {s2}, s1 is {{just kidding, s3 took ownership of it}}");
+    // note '+' uses 'add(...)' which takes an ownership and a reference
+    // it's not a matter of choice with that operator
+    // (though it seems to allow either order for the '+' version)
 
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+    println!("s is {}", s);
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("s is {}", s);
 }
