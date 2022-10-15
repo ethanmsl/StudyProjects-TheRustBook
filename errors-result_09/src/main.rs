014e945d8bff642657124ef18718d0417fae6a7e
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 16:11:20 -0700

    file read with Result type returned and then matched against

--- errors-result_09/src/main.rs
+++ errors-result_09/src/main.rs
@@ -1,3 +1,18 @@
+// enum Result<T, E> {
+//     Ok(T),
+//     Err(E),
+// }
+
+use std::fs::File;
+
+#[allow(unused_variables)]
 fn main() {
-    println!("Hello, world!");
+    // the assignment will succeed even if the file isn't found, but the
+    // variable's value will be of type Error
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
 }
