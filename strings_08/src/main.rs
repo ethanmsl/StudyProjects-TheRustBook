92a084a842426e87c90486dbd9541b32a2cd1b55
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 00:21:36 -0700

    push, concatenate, own or not

--- strings_08/src/main.rs
+++ strings_08/src/main.rs
@@ -15,4 +15,24 @@ fn main() {
 
     // (3)
     let s = String::from("initial contents");
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s2 is {}", s2);
+    // ^ note that s2 was not taken by s1
+
+    let mut s = String::from("lo");
+    s.push('l');
+    // ^ 'push' takes a char, unlike 'push_str'
+
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
+    // ^ note s1 (but not s2) has been moved here and can no longer be used
+    println!("s3 is {s3}, s2 is {s2}, s1 is {{just kidding, s3 took ownership of it}}");
+
 }
