578af57d88833ef250145ab99787ad405d28bce2
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 15:28:42 -0700

    'panic!' macro

--- error_handling_09/src/main.rs
+++ error_handling_09/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    // panic!("crash and burn");
+    // // will print:
+    // // thread 'main' panicked at 'crash and burn', src/main.rs:2:5
+    // // note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+
 }
