221351fc29ef1474ed13599e05547d7996d50446
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 00:13:48 -0700

    some literal~~>string syntaxes

--- strings_08/src/main.rs
+++ strings_08/src/main.rs
@@ -1,3 +1,18 @@
+//prevent error warnings
+// #![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+// #![allow(dead_code)]
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::new();
+
+    // (1)
+    let data = "initial contents";
+    let s = data.to_string();
+
+    // (2) the method also works on a literal directly:
+    let s = "initial contents".to_string();
+
+    // (3)
+    let s = String::from("initial contents");
 }
