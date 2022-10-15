d966b6bd3bd2d88c555a1ec4eaf7fb814dc88fb9
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 15:46:47 -0700

    comment on setting RUST_BACKTRACE=1

--- error_handling_09/src/main.rs
+++ error_handling_09/src/main.rs
@@ -12,5 +12,7 @@ fn main() {
     // // will print:
     // // thread 'main' panicked at 'index out of bounds: the len is 3 but the index is 99', src/main.rs:10:5
     // // note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+    // NOTE: you can set backtrace to on for a single run by simply 
+    //       stateing 'RUST_BACKTRACE=1' on the *same line* as, e.g., 'cargo run'
 
 }
