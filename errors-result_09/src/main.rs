4d87f6b67fcf0531930bb7c8cf2379c541a13bfb
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 16:20:00 -0700

    rather awkward to read variable error handling...

--- errors-result_09/src/main.rs
+++ errors-result_09/src/main.rs
@@ -4,6 +4,7 @@
 // }
 
 use std::fs::File;
+use std::io::ErrorKind;
 
 #[allow(unused_variables)]
 fn main() {
@@ -14,5 +15,23 @@ fn main() {
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file: {:?}", error),
+    };
+
+// ------------- handling different errors differently -------------
+    let greeting_file_result = File::open("hello.txt");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            // if 'not found' error
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                                             // ^ try to create it and...
+                Ok(fc) => fc, 
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file {:?}", other_error);
+            }
+        },
     };
 }
