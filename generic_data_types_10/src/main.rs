4c246676e7b3fab40c6c727a7317a4b7b20ba717
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-16 00:01:18 -0700

    (non-functional) Generic Type accepting largest()

--- generic_data_types_10/src/main.rs
+++ generic_data_types_10/src/main.rs
@@ -24,14 +24,33 @@ fn largest_char(list: &[char]) -> &char {
     largest
 }
 
+// generic accepting size sorter
+fn largest<T>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    };
+
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-
     let result = largest_i32(&number_list);
     println!("The largest number in {:?} is: {}", number_list, result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
-
     let result = largest_char(&char_list);
+    println!("The largest char in {:?} is: {}", char_list, result);
+
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number in {:?} is: {}", number_list, result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
     println!("The largest char in {:?} is: {}", char_list, result);
 }
