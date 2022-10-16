a859daad4c9342a4fe08d645c5b01ec7388ab72b
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 21:05:53 -0700

    Guess struct and impl, re: Ch02 guessing game

--- error_whether_to_panic_09/src/main.rs
+++ error_whether_to_panic_09/src/main.rs
@@ -1,6 +1,25 @@
 
 use std::net::IpAddr;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+     // if value < 1 || value > 100 {
+        if !(1..=100).contains(&value) {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 #[allow(unused_imports)]
 #[allow(unused_variables)]
 fn main() {
