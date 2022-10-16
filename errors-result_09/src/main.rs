9e51f1a774e839b69cd1b053ea96c7ddf9dc33bf
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 18:44:24 -0700

    commenting things out and .unwrap() & .expect("..")

--- errors-result_09/src/main.rs
+++ errors-result_09/src/main.rs
@@ -8,30 +8,41 @@ use std::io::ErrorKind;
 
 #[allow(unused_variables)]
 fn main() {
-    // the assignment will succeed even if the file isn't found, but the
-    // variable's value will be of type Error
-    let greeting_file_result = File::open("hello.txt");
-
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => panic!("Problem opening the file: {:?}", error),
-    };
+    // // the assignment will succeed even if the file isn't found, but the
+    // // variable's value will be of type Error
+    // let greeting_file_result = File::open("hello.txt");
+    //
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => panic!("Problem opening the file: {:?}", error),
+    // };
 
 // ------------- handling different errors differently -------------
-    let greeting_file_result = File::open("hello.txt");
+    // let greeting_file_result = File::open("hello.txt");
+    //
+    // let greeting_file = match greeting_file_result {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         // if 'not found' error
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //                                          // ^ try to create it and...
+    //             Ok(fc) => fc, 
+    //             Err(e) => panic!("Problem creating the file: {:?}", e),
+    //         },
+    //         other_error => {
+    //             panic!("Problem opening the file {:?}", other_error);
+    //         }
+    //     },
+    // };
 
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            // if 'not found' error
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                                             // ^ try to create it and...
-                Ok(fc) => fc, 
-                Err(e) => panic!("Problem creating the file: {:?}", e),
-            },
-            other_error => {
-                panic!("Problem opening the file {:?}", other_error);
-            }
-        },
-    };
+
+// ------------- .unwrap & .expect -------------
+    // // .unwrap() is a shortcut for the above T or panic! match
+    // let greeting_file = File::open("hello.txt").unwrap();
+
+    // // .expect("...") allows you to give more info and context in you error message
+    // let greeting_file_result = File::open("hello.txt")
+    //     .expect("hello.txt should be included in this project");
+
+
 }
