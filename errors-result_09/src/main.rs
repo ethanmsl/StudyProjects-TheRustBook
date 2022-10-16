b4f33aab5399ce3a09ec9c0d5f5a3e31e8eed1cb
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 19:18:32 -0700

    error propagation; with '?' operator

--- errors-result_09/src/main.rs
+++ errors-result_09/src/main.rs
@@ -50,8 +50,8 @@ fn main() {
 
 
 #[allow(dead_code)]
-// ------------- Propagating errors -------------
-fn read_username_from_file() -> Result<String, io::Error> {
+// ------------- Propagating errors, verbose -------------
+fn read_username_from_file_verbose() -> Result<String, io::Error> {
     let username_file_result = File::open("hello.txt");
 
     let mut username_file = match username_file_result {
@@ -68,4 +68,33 @@ fn read_username_from_file() -> Result<String, io::Error> {
         Ok(_) => Ok(username),
         Err(e) => Err(e),
     }
+}
+
+
+#[allow(dead_code)]
+// ------------- Propagating errors, '?' operator -------------
+// '?': same as the first match expresion above
+//    if Ok(t) => t
+//    if Err(e) => return Err(e)
+//  DIFFERENCE: the '?' operator will use 'from' to convert the returned error
+//              to the error type of return in the function signature
+fn read_username_from_file_quest_op() -> Result<String, io::Error> {
+    let mut username_file= File::open("hello.txt")?;
+    //                                            ^ '?'
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    //                                         ^ '?'
+
+    Ok(username)
+}
+
+
+#[allow(dead_code)]
+// ------------- Propagating errors, '?' operator w/ chaining -------------
+fn read_username_from_file_chaining() -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+
+    Ok(username)
 }
