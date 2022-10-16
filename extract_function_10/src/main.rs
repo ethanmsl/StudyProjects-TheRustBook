9e76dadf633846c4855913dce9d1fea6d75a8c68
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 23:23:24 -0700

    find largest number in vector

--- extract_function_10/src/main.rs
+++ extract_function_10/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    // definse some numbers
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    // create changing val and seed it with a start
+    let mut largest = &number_list[0];
+
+    for number in &number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number from {:?} is: {}", number_list, largest);
 }
