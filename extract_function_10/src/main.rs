f399270245af1e8b675cd7fff04573f4187e401b
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 23:33:48 -0700

    extracted code to function and used twice

--- extract_function_10/src/main.rs
+++ extract_function_10/src/main.rs
@@ -1,15 +1,23 @@
-fn main() {
-    // definse some numbers
-    let number_list = vec![34, 50, 25, 100, 65];
+fn largest(list: &[i32]) -> &i32 {
+    // create changing var and seed it with a starter val
+    let mut largest = &list[0];
 
-    // create changing val and seed it with a start
-    let mut largest = &number_list[0];
-
-    for number in &number_list {
+    for number in list {
         if number > largest {
             largest = number;
         }
-    }
+    };
 
-    println!("The largest number from {:?} is: {}", number_list, largest);
+    largest
+}
+
+fn main() {
+    // definse some numbers
+    let number_list = vec![34, 50, 25, 100, 65];
+    let largest_result = largest(&number_list);
+    println!("The largest number from {:?} is: {}", number_list, largest_result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let largest_result = largest(&number_list);
+    println!("The largest number from {:?} is: {}", number_list, largest_result);
 }
