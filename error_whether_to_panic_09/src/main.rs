667668d27fb12a05f334a6d446674303f22b728b
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 20:29:42 -0700

    documenting logic for panicing methods in them

--- error_whether_to_panic_09/src/main.rs
+++ error_whether_to_panic_09/src/main.rs
@@ -1,3 +1,14 @@
+
+use std::net::IpAddr;
+
+#[allow(unused_imports)]
+#[allow(unused_variables)]
 fn main() {
-    println!("Hello, world!");
+    // -------- expect/unwrap for compiler-opaque non-errorable code --------
+    //          Use expect("..") to document reason for beleiving non-errorable
+    //          Helpful for debugging (if incorrect), reading code, and
+    //          recognizing changes needed if changing that area of code
+    let home: IpAddr = "127.0.0.1"
+        .parse()
+        .expect("Hardcoded IP address should be valid");
 }
