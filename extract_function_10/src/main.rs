238966a79df8e9813ed5fb41382275cf1b6dce65
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-15 23:37:41 -0700

    note about variable name colliding with function name

--- extract_function_10/src/main.rs
+++ extract_function_10/src/main.rs
@@ -18,6 +18,10 @@ fn main() {
     println!("The largest number from {:?} is: {}", number_list, largest_result);
 
     let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    // NOTE: if above 'largest_result' is simply 'largest' it conflicts
+    //      with 'largest(...)' function -- indeed even rename lsp action
+    //      treats the function name as such (though treesitter highlighting
+    //      doesn't seem to have a problem...)
     let largest_result = largest(&number_list);
     println!("The largest number from {:?} is: {}", number_list, largest_result);
 }
