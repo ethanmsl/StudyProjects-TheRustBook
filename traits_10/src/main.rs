dae5ea2e5328fa542732743876bc8706118a5322
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-25 20:44:48 -0700

    placed trait, structs, and impls info in a module "aggregator"

--- traits_10/src/main.rs
+++ traits_10/src/main.rs
@@ -1,38 +1,49 @@
 #![allow(unused_variables)]
 
-pub trait Summary {
-    fn summarize(&self) -> String;
-}
+mod aggregator {
+    pub trait Summary {
+        fn summarize(&self) -> String;
+    }
 
-pub struct NewsArticle {
-    pub headline: String,
-    pub location: String,
-    pub author: String,
-    pub content: String,
-}
+    pub struct NewsArticle {
+        pub headline: String,
+        pub location: String,
+        pub author: String,
+        pub content: String,
+    }
 
-// we list the trait that we're implementing
-impl Summary for NewsArticle {
-    fn summarize(&self) -> String {
-        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    // we list the trait that we're implementing
+    impl Summary for NewsArticle {
+        fn summarize(&self) -> String {
+            format!("{}, by {} ({})", self.headline, self.author, self.location)
+        }
+    }
+
+    pub struct Tweet {
+        pub username: String,
+        pub content: String,
+        pub reply: bool,
+        pub retweet: bool,
+    }
+
+    // we list the trait that we're implementing
+    impl Summary for Tweet {
+        fn summarize(&self) -> String {
+            format!("{}: {}", self.username, self.content)
+        }
     }
 }
 
 
-pub struct Tweet {
-    pub username: String,
-    pub content: String,
-    pub reply: bool,
-    pub retweet: bool,
-}
-
-// we list the trait that we're implementing
-impl Summary for Tweet {
-    fn summarize(&self) -> String {
-        format!("{}: {}", self.username, self.content)
-    }
-}
+use aggregator::{Summary, Tweet};
 
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
