18d2b347927541bcd4234c986e1b4a3cbe20bd80
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-28 16:07:48 -0700

    added further tests, with '!' to reverse boolean state

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -36,6 +36,20 @@ mod tests {
     }
 
     #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5, 
+            height: 1,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
     fn exploration() {
         assert_eq!(2 + 2, 4);
     }
