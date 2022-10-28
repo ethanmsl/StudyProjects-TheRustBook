ced550c24dfbaadd24fd3aab12c534370f5fd773
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-28 14:47:25 -0700

    added tests for Rectangle.can_hold

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -20,15 +20,30 @@ impl Rectangle {
 mod tests {
     use super::*;
 
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5, 
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
     #[test]
     fn exploration() {
         assert_eq!(2 + 2, 4);
     }
-    
-    #[test]
-    fn another() {
-        panic!("Make this test fail");
-    }
+
+    // #[test]
+    // fn another() {
+    //     panic!("Make this test fail");
+    // }
 
     #[test]
     fn it_works() {
