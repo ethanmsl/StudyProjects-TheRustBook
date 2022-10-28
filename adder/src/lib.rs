666820e9a478b51197ff77e4cee2ecd1b0efa4b9
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-28 14:41:53 -0700

    feat: added some tests, including one that will fail

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,9 +2,21 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+// a nice little testing module
+// that's created by default on `cargo new --lib` ! :)
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+    
+    #[test]
+    fn another() {
+        panic!("Make this test fail");
+    }
 
     #[test]
     fn it_works() {
