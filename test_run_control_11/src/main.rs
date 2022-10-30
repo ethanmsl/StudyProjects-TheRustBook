bd029c0c4b707359b790649e1eebe75f4d4c48a1
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-30 13:41:01 -0700

    uncommented all the lines (note one test will fail, by design, and corresponding to its name)

--- test_run_control_11/src/main.rs
+++ test_run_control_11/src/main.rs
@@ -33,34 +33,34 @@ mod tests {
         // code that would take a long time to run
     }
 
-    // //////////////////// choosing specific tests to run //////////////////
-    // // Run tests containing phrase: `cargo test phrase`
-    // #[test]
-    // fn add_two_and_two() {
-    //     assert_eq!(4, add_two(2));
-    // }
-    //
-    // #[test]
-    // fn add_three_and_two() {
-    //     assert_eq!(5, add_two(3));
-    // }
-    //
-    // #[test]
-    // fn one_hundred() {
-    //     assert_eq!(102, add_two(100));
-    // }
+    //////////////////// choosing specific tests to run //////////////////
+    // Run tests containing phrase: `cargo test phrase`
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
 
-    // /////////////////// to compare output during tests //////////////////
-    // // Show Output even if Successful: `cargo test -- --show-output`
-    // #[test]
-    // fn this_test_will_pass() {
-    //     let value = prints_and_returns_10(4);
-    //     assert_eq!(10, value);
-    // }
-    //
-    // #[test]
-    // fn this_test_will_fail() {
-    //     let value = prints_and_returns_10(8);
-    //     assert_eq!(5, value);
-    // }
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
+    /////////////////// to compare output during tests //////////////////
+    // Show Output even if Successful: `cargo test -- --show-output`
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
+    }
 }
