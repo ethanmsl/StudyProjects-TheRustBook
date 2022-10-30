93428c3bde8eef8ee344e612ca61988e08ba74e1
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-30 14:13:05 -0700

    added /tests/integration_tests.rs and a test function

---
+++ test_organization_11/tests/integration_tests.rs
@@ -0,0 +1,9 @@
+////////////////////////// integration_tests.rs //////////////////////////
+use test_organization_11;
+
+#[test]
+fn it_adds_two() {
+    assert_eq!(4, test_organization_11::add_two(2));
+}
+
+
