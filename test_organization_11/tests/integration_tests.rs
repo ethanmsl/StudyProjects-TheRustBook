adc51e9e7d0a3f9f0fa2bb71ba7998914d4f9a51
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-30 14:29:16 -0700

    added a comment on running specific test files

--- test_organization_11/tests/integration_tests.rs
+++ test_organization_11/tests/integration_tests.rs
@@ -1,6 +1,9 @@
 ////////////////////////// integration_tests.rs //////////////////////////
 use test_organization_11;
 
+// NOTE: we can run a specific test file with `cargo test --test filename`
+//       (no `.rs` needed for the filename)
+
 #[test]
 fn it_adds_two() {
     assert_eq!(4, test_organization_11::add_two(2));
