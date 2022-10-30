cc2fa0a79de24fc599630efa14af7f27c3ba7d4b
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-30 15:13:00 -0700

    added mock helper file in a subdirectory of

---
+++ test_organization_11/tests/common/mod.rs
@@ -0,0 +1,9 @@
+////////////////// common/mod.rs /////////////////
+// Here to simulate a file of helper functions used by multiple test files
+// added as a file of a subdirectory so that it's not treated as test file itself
+// (NOTE: it's name is `mod.rs` and it's folder is `common`)
+
+pub fn setup() {
+    // setup code specific to your library's tests would go here
+}
+
--- test_organization_11/tests/integration_tests.rs
+++ test_organization_11/tests/integration_tests.rs
@@ -1,11 +1,15 @@
 ////////////////////////// integration_tests.rs //////////////////////////
 use test_organization_11;
 
+// bringing our subdirectory (same level as this file) into scope
+mod common;
+
 // NOTE: we can run a specific test file with `cargo test --test filename`
 //       (no `.rs` needed for the filename)
 
 #[test]
 fn it_adds_two() {
+    common::setup();
     assert_eq!(4, test_organization_11::add_two(2));
 }
 
