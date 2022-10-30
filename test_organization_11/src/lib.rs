933aa08ab39f8993294319b21afefd597ac3152d
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-30 14:01:16 -0700

    child modules have access to all of ancestor modules functions, thust the test module does too

--- test_organization_11/src/lib.rs
+++ test_organization_11/src/lib.rs
@@ -1,5 +1,11 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// public
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a,2)
+}
+
+// private
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 #[cfg(test)]
@@ -7,8 +13,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    // tests module can access the private function becuase
+    // *universally*: child modules can use all of ancestor modules' functions
+    fn internal() {
+        assert_eq!(4, internal_adder(2,2));
     }
 }
