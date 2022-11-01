af82047b3a3e8c0707a04ce53fe825721d5869c2
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-31 21:08:10 -0700

    added some blank lines for enhanced readability

--- minigrep_12/src/main.rs
+++ minigrep_12/src/main.rs
@@ -10,12 +10,14 @@ fn main() {
 
     let (query, file_path) = parse_config(&args);
 
+    println!();
     println!("Searching for {}", query);
     println!("In file {}", file_path);
 
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
+    println!();
     println!("With text:\n{contents}");
 }
 
