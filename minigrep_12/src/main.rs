4ba46e744365159b783bdb462ce169e9f45ee6a0
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-31 21:07:09 -0700

    extracted parameter parsing to a function & moved some comments

--- minigrep_12/src/main.rs
+++ minigrep_12/src/main.rs
@@ -1,20 +1,14 @@
 use std::env;
 use std::fs;
 
+// args = dbg!(args);
+// QUESTION: ^ args is immutable, if I give it's value away is ther anyway
+//             for it to get ownership back??
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // ^ note: the first argument will be the name of the program that collects the arguments
-    // e.g., in this case, "target/debug/minigrep_12"
-    //                       ^ NOTE: I'm not sure why "target" is there, probably
-    //                               something to do with where the binary is kept
-    dbg!(&args);
-    // args = dbg!(args);
-    // QUESTION: ^ args is immutable, if I give it's value away is ther anyway
-    //             for it to get ownership back??
 
-    // ignore calling_program = &args[0];
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", file_path);
@@ -23,4 +17,12 @@ fn main() {
         .expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    // ignore calling_program = &args[0];
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
