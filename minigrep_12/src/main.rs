fcd2a3055995040b37c0d6a4a55f9f222a69f1ed
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-31 21:21:02 -0700

    changing tuple of values to a 'Config'-Struct

--- minigrep_12/src/main.rs
+++ minigrep_12/src/main.rs
@@ -8,23 +8,28 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
     println!();
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!();
     println!("With text:\n{contents}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    // ignore calling_program = &args[0];
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+fn parse_config(args: &[String]) -> Config {
+    // ignore calling_program = &args[0];
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
