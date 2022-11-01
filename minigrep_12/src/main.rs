0d7648ec055af2f00ddea58f6aa08d0f85394df8
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-31 19:59:00 -0700

    put values of interest in named variables; printing them instead of debug

--- minigrep_12/src/main.rs
+++ minigrep_12/src/main.rs
@@ -6,5 +6,11 @@ fn main() {
     // e.g., in this case, "target/debug/minigrep_12"
     //                       ^ NOTE: I'm not sure why "target" is there, probably
     //                               something to do with where the binary is kept
-    dbg!(args);
+
+    // ignore calling_program = &args[0];
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
 }
