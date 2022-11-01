fe913d33ce21633822dcb7333bf1db952dac9319
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-10-31 19:53:59 -0700

    comment: on behavior of 'env::args()'

--- minigrep_12/src/main.rs
+++ minigrep_12/src/main.rs
@@ -2,5 +2,9 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    // ^ note: the first argument will be the name of the program that collects the arguments
+    // e.g., in this case, "target/debug/minigrep_12"
+    //                       ^ NOTE: I'm not sure why "target" is there, probably
+    //                               something to do with where the binary is kept
     dbg!(args);
 }
