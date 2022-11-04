70476e2522040addcba8e1cc133508bb21f73d73
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-04 04:48:18 -0700

    feat & format: can now select case-sensitivity appropriate search function via ENV variable; also auto-format changes

--- minigrep_12/src/lib.rs
+++ minigrep_12/src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -16,15 +18,29 @@ impl Config {
         // Passed Guardian(s) //
         let query = args[1].clone();
         let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        //                                         ^ True on Ok()
+        //                                         & False on Err()
 
-        Ok(Config { query, file_path })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    // chec config and call case-sensitivity-appropriate search function
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -83,6 +99,9 @@ safe, fast, productive.
 Pick three.
 Trust me.";
 
-        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
