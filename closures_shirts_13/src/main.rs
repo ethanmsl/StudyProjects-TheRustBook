2b8ac0f545c253120c5d1d68266ecc1e2fb4e5fe
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-06 09:42:31 -0800

    writing to outside variable with a closure without taking and transfering ownership

--- closures_shirts_13/src/main.rs
+++ closures_shirts_13/src/main.rs
@@ -155,4 +155,20 @@ fn main() {
     //     r.width
     // });
     // println!("{:#?}", list);
+
+    let mut list = [
+        Rectangle { width: 10, height: 1  },
+        Rectangle { width: 3,  height: 5  },
+        Rectangle { width: 7,  height: 12 },
+    ];
+
+    let mut num_sort_operations = 0;
+    list.sort_by_key(|r| {
+        num_sort_operations += 1;
+        r.width
+    });
+    println!("{:#?}, sorted in {} operations", list, num_sort_operations);
+
+
+
 }
