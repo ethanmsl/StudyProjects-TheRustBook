b5d55e9a131ab2098312ea4fe813adee61a39254
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-06 10:11:21 -0800

    "tests" showing function of .next() and comments on mutability and iterator use

--- iterators_13/src/main.rs
+++ iterators_13/src/main.rs
@@ -9,8 +9,27 @@
 fn main() {
     let v1 = vec![1, 2, 3];
     let v1_iter = v1.iter();
+    //  ^ NOTE: that while immutable, when used in the for loop below
+    //          ownership is given tot the for loop 
+    //          and mutability is effectively declared there
 
     for val in v1_iter {
         println!("Got: {}", val);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn iterstor_demonstration() {
+        let v1 = vec![1, 2, 3,];
+
+        let mut v1_iter = v1.iter();
+        //   ^ NOTE: mutable, needed to manually use .next()
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None    );
     }
 }
