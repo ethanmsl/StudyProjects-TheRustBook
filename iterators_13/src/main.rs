68f4720e8f44332421b4ea84ab4df4041621f193
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-06 12:03:55 -0800

    test using .iter().filter(...).collect()

--- iterators_13/src/main.rs
+++ iterators_13/src/main.rs
@@ -33,8 +33,21 @@ fn main() {
     }
 }
 
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter()
+        .filter(|s| s.size == shoe_size).collect()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn iterstor_demonstration() {
         let v1 = vec![1, 2, 3,];
@@ -66,5 +79,25 @@ mod tests {
         //        ^ this type annotation was needed
 
         assert_eq!(v2, vec![2, 3, 4,]);
+    }
+
+    #[test]
+    fn filter_by_size() {
+        let shoes = vec![
+            Shoe { size: 10, style: String::from("sneaker") },
+            Shoe { size: 13, style: String::from("sandal") },
+            Shoe { size: 10, style: String::from("boot") },
+        ];
+
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe { size: 10, style: String::from("sneaker") },
+                Shoe { size: 10, style: String::from("boot") },
+            ]
+        );
     }
 }
