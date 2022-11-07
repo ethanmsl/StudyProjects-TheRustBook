e6f639891a49538fe3d694f54222a928d85d5537
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-06 21:37:06 -0800

    testing pulling out files and doing stuff with them

--- rename_directories-NULL/src/main.rs
+++ rename_directories-NULL/src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs;
+use std::fs::rename;
 
 use rename_directories_NULL as lib;
 
@@ -8,6 +10,20 @@ fn main() -> std::io::Result<()> {
     // // intended use path (one-off prod)
     // let path_prepend = String::from("/Users/eskowronski-lutz/Documents/Programming_Langs/Rust/book-projects-rust");
     let args_iterator = env::args();
-
-    lib::run(args_iterator, path_prepend)
+    let files: Vec<_> = 
+        fs::read_dir(&path_prepend)?
+        .map(|path| path.unwrap().path())
+        .collect();
+    println!("files: {:?}", files[3]);
+    if files[3].to_str() == Some("./a.txt") {
+        println!("is_dir");
+        fs::rename(&files[3], &files[3].with_file_name("b.txt"))?;
+    }
+    // for elem in files {
+    //     println!("elem: {:?}", elem?.path());
+    // }
+    // let files = fs::read_dir(&path_prepend)?;
+    // fs:rename(from, to)
+    // lib::run(args_iterator, path_prepend)
+    Ok(())
 }
