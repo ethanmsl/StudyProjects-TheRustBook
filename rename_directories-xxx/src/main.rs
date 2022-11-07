23d03ca0edd5bcfacf28960bfd6798492538ec5a
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-07 13:16:43 -0800

    cleaning and assigning 'path_prepend' var conditionally

--- rename_directories-xxx/src/main.rs
+++ rename_directories-xxx/src/main.rs
@@ -1,33 +1,33 @@
 use std::env;
 use std::fs;
 
-use rename_directories_xxx as lib;
-use lib::test_prints::print_wave;
 use lib::arg_parse;
+use lib::test_prints::print_wave;
+use rename_directories_xxx as lib;
 
 fn main() -> std::io::Result<()> {
+    // //////////////////////// test prints ////////////////////////
     // exploring how to call objects with modularized code
     lib::test_prints::print_hello(); // because can call 'lib'
     lib::print_bye();                // because lib has a use statement bringing in 'print_bye()'
     print_wave();                    // becuase we have a use statment from 'lib::...' in this file
-    // test_prints::print_bye();        // Not Valid
-    // print_bye();                     // Not Valid
+    // test_prints::print_bye();     // Not Valid
+    // print_bye();                  // Not Valid
+    // ////////////////////////////////////////////////////////////
 
-
+    // //////////////////////// arg parse ////////////////////////
     // get args, first should be path, if second we'll see if it's a path
     let args_iterator = env::args();
     let maybe_path = arg_parse::check_for_path_argument(args_iterator);
-    if let Some(path) = maybe_path {
+    let path_prepend = if let Some(path) = maybe_path {
         println!("Path: {}", path);
+        path
     } else {
         println!("No path provided");
-    }
-    //
-    //
-    // local path (test)
-    let path_prepend = String::from(".");
-    // // intended use path (one-off prod)
-    // let path_prepend = String::from("/Users/eskowronski-lutz/Documents/Programming_Langs/Rust/book-projects-rust");
+        String::from(".")
+    };
+    dbg!(&path_prepend);
+    // ////////////////////////////////////////////////////////////
 
     let files_iterator = fs::read_dir(&path_prepend)?;
     lib::files_print_swaps(files_iterator);
