823f377eb9eff61aba2a097b634f1babe2734784
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-07 15:26:27 -0800

    added further tests and refactored function to make guardianship work (and thus assist with testing as well)

--- rename_directories-xxx/src/to_from.rs
+++ rename_directories-xxx/src/to_from.rs
@@ -11,20 +11,31 @@ impl ToFromPair {
     }
 
     pub fn from_args(mut args: impl Iterator<Item = String>, prepend: &str) -> ToFromPair {
-        // Guardian
+        // Guardian 1
         if prepend.ends_with('/') {
             panic!("prepend should not end with a '/'\nthe code assumes this came from a sanitized input");
         }
-        // additional guardianship of path formatting could be added here
-        // however this function is probably already deprecated, so I likely won't
 
-        // Passed Guardian
+        // diagnostic
         let curr_dir = args.next().unwrap();
         println!("curr_dir: {}", curr_dir);
         println!("prepend: {}", prepend);
 
-        let from_arg = format!("{}/{}", prepend, args.next().unwrap());
-        let to_arg   = format!("{}/{}", prepend, args.next().unwrap());
+        // extract
+        let from_raw = args.next().unwrap();
+        let to_raw = args.next().unwrap();
+
+        // Guardian 2
+        if from_raw.starts_with('/') || to_raw.starts_with('/') {
+            panic!("neither from_raw nor to_raw.to should begin with a '/'");
+        }
+        let from_arg = format!("{}/{}", prepend, from_raw);
+        let to_arg   = format!("{}/{}", prepend, to_raw);
+
+        // additional guardianship of path formatting could be added here
+        // however this function is probably already deprecated, so I likely won't
+
+
 
         ToFromPair::new(from_arg, to_arg)
     }
@@ -50,5 +61,27 @@ mod tests {
         let pair_struct = ToFromPair::from_args(args.into_iter(), "prepend");
         assert_eq!(pair_struct.from, "prepend/from");
         assert_eq!(pair_struct.to, "prepend/to");
+    }
+
+
+    #[test]
+    #[should_panic]
+    fn to_from_pair_from_args_prepend_broken() {
+        let args = vec!["curr_dir".to_string(), "from".to_string(), "to".to_string()];
+        ToFromPair::from_args(args.into_iter(), "prepend/");
+    }
+
+    #[test]
+    #[should_panic]
+    fn to_from_pair_from_args_from_broken() {
+        let args = vec!["curr_dir".to_string(), "/from".to_string(), "to".to_string()];
+        ToFromPair::from_args(args.into_iter(), "prepend");
+    }
+
+    #[test]
+    #[should_panic]
+    fn to_from_pair_from_args_to_broken() {
+        let args = vec!["curr_dir".to_string(), "from".to_string(), "/to".to_string()];
+        ToFromPair::from_args(args.into_iter(), "prepend");
     }
 }
