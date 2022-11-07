dcb871b1871c6864bf31ed54f8a55a0c9f65b058
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-06 18:49:38 -0800

    swap dashes and underscores function and a test for it

--- rename_directories-NULL/src/lib.rs
+++ rename_directories-NULL/src/lib.rs
@@ -1,6 +1,6 @@
 #[derive(Debug)]
 pub struct ToFromPair {
-    pub to:   String,
+    pub to: String,
     pub from: String,
 }
 
@@ -12,12 +12,35 @@ impl ToFromPair {
     pub fn from_args(mut args: impl Iterator<Item = String>, prepend: &str) -> ToFromPair {
         let curr_dir = args.next().unwrap();
         println!("curr_dir: {}", curr_dir);
-        println!("prepend: {}",  prepend);
+        println!("prepend: {}", prepend);
 
-        let to_arg   = format!("{}/{}", prepend, args.next().unwrap());
+        let to_arg = format!("{}/{}", prepend, args.next().unwrap());
         let from_arg = format!("{}/{}", prepend, args.next().unwrap());
 
         ToFromPair::new(to_arg, from_arg)
     }
 }
 
+pub fn swap_dashes_and_underscores(input: String) -> String {
+    input
+        .chars()
+        .map(|c| match c {
+            '-' => '_',
+            '_' => '-',
+            _ => c,
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn line_swaps() {
+        let input = String::from("hello-world and -_---_-");
+        let expected = String::from("hello_world and _-___-_");
+        let actual = swap_dashes_and_underscores(input);
+        assert_eq!(expected, actual);
+    }
+}
