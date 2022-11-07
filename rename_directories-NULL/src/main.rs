b0f014a00c081d926c79762bde8e9dc04342bc53
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-06 22:07:24 -0800

    various code swapping extracted file path names

--- rename_directories-NULL/src/main.rs
+++ rename_directories-NULL/src/main.rs
@@ -14,13 +14,22 @@ fn main() -> std::io::Result<()> {
         fs::read_dir(&path_prepend)?
         .map(|path| path.unwrap().path())
         .collect();
-    println!("files: {:?}", files[3]);
-    if files[3].to_str() == Some("./a.txt") {
-        println!("matched 'a.txt'");
-        fs::rename(&files[3], &files[3].with_file_name("b.txt"))?;
-    } else {
-        println!("did not match 'a.txt'");
+    let files_iterator = fs::read_dir(&path_prepend)?;
+    for file in files_iterator {
+        let file = file.unwrap().path();
+        let file_ref = file.to_str().unwrap();
+        println!("file o.: {:?}", file_ref);
+        let changed = lib::swap_dashes_and_underscores(file_ref);
+        println!("changed: {:?}", changed);
+
     }
+    
+    // if files[3].to_str() == Some("./a.txt") {
+    //     println!("matched 'a.txt'");
+    //     fs::rename(&files[3], &files[3].with_file_name("b.txt"))?;
+    // } else {
+    //     println!("did not match 'a.txt'");
+    // }
 
     // lib::run(args_iterator, path_prepend)
     Ok(())
