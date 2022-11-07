09dc77c8c089fe564f0f2c2a4451cbb05918b8c5
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-07 14:05:18 -0800

    better comments

--- rename_directories-xxx/src/arg_parse.rs
+++ rename_directories-xxx/src/arg_parse.rs
@@ -11,7 +11,7 @@ pub fn check_for_path_argument(args_iterator: std::env::Args) -> Option<String> 
     let mut args = args_iterator;
     args.next(); // skip the first arg, which is the path to the executable
 
-    //NOTE: .map() on an Option returns a Some() on a Some() and None on a None
-    //      .... I *infer*
+    //NOTE: .map() on an Option returns a Some() on a Some() and None on a None (...I think)
+    // Sanitize possible ending '/'s
     args.next().map(|path| path.trim_end_matches('/').to_string())
 }
