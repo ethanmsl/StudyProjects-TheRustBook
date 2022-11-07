a53dffaff304a2a8a087216cfb24738882c8dc7e
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-06 22:31:13 -0800

    commented out some args related elements used for early exploration and testing

--- rename_directories-xxx/src/main.rs
+++ rename_directories-xxx/src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+// use std::env;
 use std::fs;
 
 use rename_directories_xxx as lib;
@@ -8,7 +8,8 @@ fn main() -> std::io::Result<()> {
     let path_prepend = String::from(".");
     // // intended use path (one-off prod)
     // let path_prepend = String::from("/Users/eskowronski-lutz/Documents/Programming_Langs/Rust/book-projects-rust");
-    let args_iterator = env::args();
+
+    // let args_iterator = env::args();
     let files_iterator = fs::read_dir(&path_prepend)?;
     lib::files_print_swaps(files_iterator);
     // lib::run(args_iterator, path_prepend)
