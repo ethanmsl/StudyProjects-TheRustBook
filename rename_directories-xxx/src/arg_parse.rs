44990e755b64e4f2a89885cc320c649dfff56faa
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-07 14:55:52 -0800

    commented on testing issues I've yet to conquer with Args using functions

--- rename_directories-xxx/src/arg_parse.rs
+++ rename_directories-xxx/src/arg_parse.rs
@@ -15,3 +15,12 @@ pub fn check_for_path_argument(args_iterator: std::env::Args) -> Option<String> 
     // Sanitize possible ending '/'s
     args.next().map(|path| path.trim_end_matches('/').to_string())
 }
+
+
+#[cfg(test)]
+mod tests {
+    // Note sure how to test this given that it takes in an 'Args' type
+    // looking earlier I didn't see a nice way to artificiall generate an Args object
+    // nor am I sure of a good way to artificially control that environment
+    // as part of testing (so that regular Args generating machinery could be employed)
+}
