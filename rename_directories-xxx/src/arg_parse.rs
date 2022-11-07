81801c2afb89afef5d87e3d8b30ddee6e784ddef
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-07 12:22:34 -0800

    elaborated error message and auto-formatted

--- rename_directories-xxx/src/arg_parse.rs
+++ rename_directories-xxx/src/arg_parse.rs
@@ -1,7 +1,10 @@
 pub fn check_for_path_argument(args_iterator: std::env::Args) -> Option<String> {
     // Guardian
     if args_iterator.len() != 2 {
-        eprintln!("Error: Expected 1 argument, got {}", args_iterator.len()-1);
+        eprintln!(
+            "Error: Expected 1 valid path argument to be explicitely provided, got {}",
+            args_iterator.len() - 1
+        );
         return None;
     }
     // Passed Guardian
