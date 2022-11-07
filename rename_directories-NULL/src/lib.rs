0dc693521f8d27349acc16fe13eb256731d4dfac
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-06 18:58:06 -0800

    refactor: swapped "from" and "to" and added match for broader functionality

--- rename_directories-NULL/src/lib.rs
+++ rename_directories-NULL/src/lib.rs
@@ -1,12 +1,12 @@
 #[derive(Debug)]
 pub struct ToFromPair {
-    pub to: String,
     pub from: String,
+    pub to: String,
 }
 
 impl ToFromPair {
-    fn new(to: String, from: String) -> ToFromPair {
-        ToFromPair { to, from }
+    fn new(from: String, to: String,) -> ToFromPair {
+        ToFromPair { from, to }
     }
 
     pub fn from_args(mut args: impl Iterator<Item = String>, prepend: &str) -> ToFromPair {
@@ -14,10 +14,10 @@ impl ToFromPair {
         println!("curr_dir: {}", curr_dir);
         println!("prepend: {}", prepend);
 
-        let to_arg = format!("{}/{}", prepend, args.next().unwrap());
         let from_arg = format!("{}/{}", prepend, args.next().unwrap());
+        let to_arg = format!("{}/{}", prepend, args.next().unwrap());
 
-        ToFromPair::new(to_arg, from_arg)
+        ToFromPair::new(from_arg, to_arg,)
     }
 }
 
--- rename_directories-NULL/src/main.rs
+++ rename_directories-NULL/src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 
+use rename_directories_NULL as lib;
 use rename_directories_NULL::ToFromPair;
 
 fn main() -> std::io::Result<()> {
@@ -8,12 +9,16 @@ fn main() -> std::io::Result<()> {
     let path_prepend = String::from(".");
     // // intended use path (one-off prod)
     // let path_prepend = String::from("/Users/eskowronski-lutz/Documents/Programming_Langs/Rust/book-projects-rust");
-
     let args_iterator = env::args();
-    let to_from = ToFromPair::from_args(args_iterator, &path_prepend);
+    let arg_length = args_iterator.len();
 
-    println!("to: {:?}", to_from);
-
-    fs::rename(to_from.to, to_from.from)?;
+    match arg_length {
+        3 => {
+                let to_from = ToFromPair::from_args(args_iterator, &path_prepend);
+                println!("to: {:?}", to_from);
+                fs::rename(to_from.from, to_from.to,)?;
+        },
+        _ => println!("Please provide two arguments: the directory to rename and the new name for the directory."),
+    }
     Ok(())
 }
