7490a11759e11c75096ff0040ca3968083c92756
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-06 16:59:49 -0800

    offered two hardcoded path prepends, via commenting/uncommenting

--- rename_directories-NULL/src/main.rs
+++ rename_directories-NULL/src/main.rs
@@ -30,8 +30,12 @@ impl ToFromPair {
     // }
 
 fn main() -> std::io::Result<()> {
+    // local path (test)
+    let path_prepend = String::from(".");
+    // // intended use path (one-off prod)
+    // let path_prepend = String::from("/Users/eskowronski-lutz/Documents/Programming_Langs/Rust/book-projects-rust");
+
     let args_iterator = env::args();
-    let path_prepend = String::from("/Users/eskowronski-lutz/Documents/Programming_Langs/Rust/book-projects-rust");
     let to_from = ToFromPair::from_args(args_iterator, &path_prepend);
 
     println!("to: {:?}", to_from);
