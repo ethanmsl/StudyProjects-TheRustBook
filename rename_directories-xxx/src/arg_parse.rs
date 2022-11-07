97456511d294b2d825ea2af82f475257fd6311de
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-07 12:21:29 -0800

    added number of arguments guardian

--- rename_directories-xxx/src/arg_parse.rs
+++ rename_directories-xxx/src/arg_parse.rs
@@ -1,4 +1,10 @@
 pub fn check_for_path_argument(args_iterator: std::env::Args) -> Option<String> {
+    // Guardian
+    if args_iterator.len() != 2 {
+        eprintln!("Error: Expected 1 argument, got {}", args_iterator.len()-1);
+        return None;
+    }
+    // Passed Guardian
     let mut args = args_iterator;
     args.next(); // skip the first arg, which is the path to the executable
     args.next()
