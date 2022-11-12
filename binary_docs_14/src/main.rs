b818e896059a6e35e5b53dc71808654815853aa8
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-12 15:27:49 -0800

    doc comments in binary (main.rs, no lib.rs) crate

--- binary_docs_14/src/main.rs
+++ binary_docs_14/src/main.rs
@@ -1,3 +1,17 @@
+//! A Test of Whether Docs are Produced for Binary (main.rs only) Files
+//! ================================================================
+//! # How are you?
+//! - I'm fine, thanks.
+//!  - How are you?
+//!   - Also fine, thanks.
+
+
+/// The main function.
+/// # Examples
+///
+/// ```
+/// println!("Hello, world!");
+/// ```
 fn main() {
     println!("Hello, world!");
 }
