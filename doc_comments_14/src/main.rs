e5aa4af11a999d8f33ee78d0e43fede32a01c098
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-12 14:26:15 -0800

    functions with comments
    use `cargo docs --open` to see, or just `cargo docs` to generate in the
    `target/doc` directory

--- doc_comments_14/src/main.rs
+++ doc_comments_14/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
     println!("Hello, world!");
 }
+
+/// Adds one to the number given.
+///
+/// # Examples
+/// ```
+/// let arg = 5
+/// let answer = doc_comments_14::add_one(arg);
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+/// While this prints to screen, it's really just here to write aboud and use doc-comments
+///
+/// # Common Sections:
+/// ## Examples
+/// ## Panics
+/// ## Errors
+/// ## Safety
+pub fn just_for_the_docs() {
+    println!("This is just for the docs");
+}
