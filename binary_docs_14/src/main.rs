ed33e430852594402bb0c3f6f54bc411c8d6f8ad
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-12 15:35:45 -0800

    observations on overflow and build

--- binary_docs_14/src/main.rs
+++ binary_docs_14/src/main.rs
@@ -14,4 +14,29 @@
 /// ```
 fn main() {
     println!("Hello, world!");
+    println!(" 2147483647 * 6 = {}", a_mult_b(2147483647, 6));
+}
+
+
+/// Multiplies two numbers.
+///
+/// # Examples
+/// ```
+/// let x = 2147483647;
+/// let y = 6;
+/// assert_eq!(30, multiply(x, y));
+/// ```
+///
+/// # Panics
+/// This function will panic if the result overflows a `i32`.
+///  ^ **NOTE**: I don't know if that's true, that was co-pilot auto-generated
+/// 
+/// As suspected/dimly remembered:
+/// it causes a panic **in dev mode**
+/// , but mereley 'wraps' in release mode.
+/// e.g. `cargo run` panics (as we do the above example in main()
+/// , but `cargo run --release` does not. (and instead procduces `-6`)
+
+fn a_mult_b(a: i32, b: i32) -> i32 {
+    a * b
 }
