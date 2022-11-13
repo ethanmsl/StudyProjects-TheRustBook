10b02b46b5da9fc91d40bf85dc37f752eda51741
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-13 13:14:47 -0800

    non-compiling code example of attempted recursive type Enum implementation

--- smart_pointers_15/src/recursive.rs
+++ smart_pointers_15/src/recursive.rs
@@ -4,7 +4,18 @@
 //! Seems to consist of pairs of pointers that generally point to a non-cons value
 //! and another cons element
 //!
-//! # Conceptual Example
-//! pseudo-code representation of a cons list containing 1,2, and 3:
-//!     `(1, (2, (3, (Nil))))`
-//!
+
+
+/// A cons list -- recursive list type
+///
+/// # Conceptual Example
+/// pseudo-code representation of a cons list containing 1,2, and 3:
+///     `(1, (2, (3, (Nil))))`
+///
+/// # Verbiage Note:
+/// `Nil` is the canonical name for the terminating or "base case" of at least some recursions
+enum List {
+    Cons(i32, List),
+    Nil,
+}
+// NOTE: ^ The above is an example of a NON-compiling attempt at a recursive type
