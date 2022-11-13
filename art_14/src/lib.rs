e8359c730051b039cc202eea979d9c138de9dd26
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-12 16:58:47 -0800

    multi-module library -- that, as is, documents itself and is accessible only by users referencing its internal structure

--- art_14/src/lib.rs
+++ art_14/src/lib.rs
@@ -1,14 +1,38 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! # Art
+//!
+//! A library for modeling artistic concepts.
+
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors according to the RYB color model.
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod utils {
+    use crate::kinds::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// Combines two primary colors in equal amounts to create
+    /// a secondary color.
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        match (c1, c2) {
+            (PrimaryColor::Red, PrimaryColor::Yellow)
+            | (PrimaryColor::Yellow, PrimaryColor::Red) => SecondaryColor::Orange,
+            (PrimaryColor::Red, PrimaryColor::Blue) | (PrimaryColor::Blue, PrimaryColor::Red) => {
+                SecondaryColor::Purple
+            }
+            (PrimaryColor::Blue, PrimaryColor::Yellow)
+            | (PrimaryColor::Yellow, PrimaryColor::Blue) => SecondaryColor::Green,
+            _ => SecondaryColor::Purple
+        }
     }
 }
