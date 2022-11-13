2c5d9e1a280e65cdb3f0c4a3b81b63795c0af537
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-13 11:01:45 -0800

    rescustive types, cons lists, doc comments

--- smart_pointers_15/src/main.rs
+++ smart_pointers_15/src/main.rs
@@ -1,5 +1,6 @@
 //! Ch. 15. Smart Pointers - "The Rust Programming Language"
 
+mod recursive;
 
 /// the function that serves as insertion to run
 fn main() {
---
+++ smart_pointers_15/src/recursive.rs
@@ -0,0 +1,10 @@
+//! A file to work with recursive types using Box types
+//! the particular recursive type of interest is what is known as a
+//! **Cons List**, common in Lisp and apparently other functional programming languages
+//! Seems to consist of pairs of pointers that generally point to a non-cons value
+//! and another cons element
+//!
+//! # Conceptual Example
+//! pseudo-code representation of a cons list containing 1,2, and 3:
+//!     `(1, (2, (3, (Null))))`
+//!
