7438b37f9ae87918b28e0ba82cfe08ab9d26b3ad
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-11-26 14:57:55 -0800

    hmmm, not as described ... to investigate more about struct lifetimes later

--- struct-lifetimes_XX/src/main.rs
+++ struct-lifetimes_XX/src/main.rs
@@ -1,3 +1,30 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+// Hmm... a post here
+// (https://users.rust-lang.org/t/what-is-the-point-of-lifetime-parameters-in-struct-impl-blocks/14631/8)
+// said that the code below wouldn't compile and I didn't see why ... but it does compile... (¿maybe
+// it wouldn't in 2020?)
 fn main() {
-    println!("Hello, world!");
+    struct Foo<'a> {
+        x: &'a i32,
+    }
+
+    struct Bar<'a> {
+        f1: &'a mut Foo<'a>,
+        f2: &'a Foo<'a>,
+    }
+
+    fn main() {
+        let x = &20;
+        let mut foo = Foo { x };
+
+        let x2 = &10;
+
+        let mut foo2 = Foo { x: x2 };
+        let bar = Bar {
+            f1: &mut foo,
+            f2: &foo2,
+        };
+    }
 }
