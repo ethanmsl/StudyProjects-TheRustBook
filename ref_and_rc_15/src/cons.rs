5017baa73d65fbd5a16688c9550400448890168f
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-05 19:07:01 -0800

    checking that I can call tests in non-main.rs & non-lib.rs code

---
+++ ref_and_rc_15/src/cons.rs
@@ -0,0 +1,13 @@
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_to_test_if_tests_test() {
+        assert_eq!(2+2, 4);
+    }
+
+}
+
--- ref_and_rc_15/src/main.rs
+++ ref_and_rc_15/src/main.rs
@@ -1,3 +1,5 @@
+mod cons;
+
 fn main() {
     println!("Hello, world!");
 }
