aa8dbf06cb0e354ecde636f34a6f9ca493a0ddca
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-05 18:17:07 -0800

    Example of panicking RefCell<T> code
    Note: that merely allotting a second mutable borrow even without using it
    caused a panic

--- mock-object-refcell_15/src/lib.rs
+++ mock-object-refcell_15/src/lib.rs
@@ -56,11 +56,30 @@ mod tests {
         }
     }
 
+    // Working Messenger implementation
     impl Messenger for MockMessenger {
         fn send(&self, message: &str) {
             self.sent_messages.borrow_mut().push(String::from(message));
         }
     }
+
+    // // Panic-inducing Messenger implementation
+    // // Can be compiled because RefCell<T> does borrow-checking at runtime
+    // impl Messenger for MockMessenger {
+    //     fn send(&self, message: &str) {
+    //         let mut one_borrow = self.sent_messages.borrow_mut();
+    //         let mut two_borrow = self.sent_messages.borrow_mut();
+    //         // ^ Panicks at second co-mutable assignment
+    //
+    //         one_borrow.push(String::from(message));
+    //         // two_borrow.push(String::from(message));
+    //         // ^ even without use the above will panic
+    //         // (which turns out to be how compilation mutability works too
+    //         // I was thinking it would check for use -- I think that may
+    //         // onlybe in the case re: looking via immutable ref after mutating)
+    //         //  ^ which I need to check how RefCell<T> handles
+    //     }
+    // }
 
     #[test]
     fn it_sends_an_over_75_percent_warning_message() {
@@ -70,5 +89,25 @@ mod tests {
         limit_tracker.set_value(80);
 
         assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
+
+    #[test]
+    fn verifying_compiler_looks_at_mutable_reference_usage() {
+        let mut vev = vec![1, 2, 3];
+        let mut vev_1 = &mut vev;
+        // let mut vev_2 = &mut vev;
+        // ^ this does prevent compilation even if not used
+
+        let mut boop = vec![3, 2, 1];
+        vev_1 = &mut boop;
+        assert_eq!(*vev_1, vec![3, 2, 1]);
+
+        // this does not require vev_1 to be mutable
+        // vev_1.push(4);
+        // assert_eq!(vev, vec![1,2,3,4]);
+
+        // this does not require vev_1 to be mutable
+        // vev_1[2] = 5;
+        // assert_eq!(vev, vec![1,2,5]);
     }
 }
