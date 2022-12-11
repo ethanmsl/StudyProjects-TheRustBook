aa28e864c1060de2882243cc7dccf04693fda027
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-11 07:53:19 -0800

    thread spawning and print racing

--- concurrent_threads_16/src/main.rs
+++ concurrent_threads_16/src/main.rs
@@ -1,3 +1,20 @@
+//! 16.1 -- Using Threads to Run Code Simultaneously
+
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    thread::spawn(|| {
+        for i in 1..=20 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(2));
+        //                                  ^ can play with to affect threads
+        //                                    from above that occur
+    }
 }
