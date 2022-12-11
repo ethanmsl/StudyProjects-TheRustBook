7e5fbb61a931bdbc3078517d1fd54e8f87409ef4
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-11 14:02:12 -0800

    move closure and thread

--- concurrent_threads_joinhandle_16/src/main.rs
+++ concurrent_threads_joinhandle_16/src/main.rs
@@ -6,28 +6,46 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let handle_a = thread::spawn(|| {
-        for i in 1..10 {
-            println!("- spawn-thrd--A--: loop number ##{}##", i);
+    println!("----------------------------------------\n");
+    {
+        // Spawn two threads, join one to other then the other to main
+
+        let handle_a = thread::spawn(|| {
+            for i in 1..10 {
+                println!("- spawn-thrd--A--: loop number ##{}##", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+
+        let handle_b = thread::spawn(|| {
+            for i in 1..10 {
+                println!("- spawn-thrd--B--: loop number @ {} @", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+            handle_a.join().unwrap();
+            // ^ joining thread_a to thread_b
+        });
+
+        for i in 1..5 {
+            println!("main thread: loop number {}", i);
             thread::sleep(Duration::from_millis(1));
         }
-    });
 
-    let handle_b = thread::spawn(|| {
-        for i in 1..10 {
-            println!("- spawn-thrd--B--: loop number @ {} @", i);
-            thread::sleep(Duration::from_millis(1));
-        }
-        handle_a.join().unwrap();
-        // ^ joining thread_a to thread_b
-    });
-
-    for i in 1..5 {
-        println!("main thread: loop number {}", i);
-        thread::sleep(Duration::from_millis(1));
+        let _boop = handle_b.join();
+        //  ^               ^ joinging thread_b, which has thread_a joined to it
+        //  ^ a Result is returned - partly just `()`, but also something new...
     }
+    println!("----------------------------------------\n");
+    {
+        // Closures and Threads
 
-    let _boop = handle_b.join();
-    //  ^               ^ joinging thread_b, which has thread_a joined to it
-    //  ^ a Result is returned - partly just `()`, but also something new...
+        let v = vec![1, 2, 3];
+
+        let handle = thread::spawn( move || {
+            //                       ^ without this the closure won't own `v`
+            //                         and thus may outlive what it references
+            println!("Here's a vector: {:?}", v);
+        });
+        handle.join().unwrap();
+    }
 }
