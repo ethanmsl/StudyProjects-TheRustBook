d4b3fd93b4f690d61889ed588543d48dad67705e
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-11 15:26:36 -0800

    vector ref with range via two other captured variable
    thought I had basically the same thing before, but it didn't work --
    presumably I was getting errors for something else (like not accessing
    vec as a ref) and had other issues introduced in my corrections)

--- closure_and_function_explorer_xx/src/main.rs
+++ closure_and_function_explorer_xx/src/main.rs
@@ -52,7 +52,7 @@ fn main() {
     println!("----------------------------------------\n");
 
     let mut vec = vec![1, 2, 3, 4, 5];
-    vec[0] += 10;  // Fine, before closure even touches it
+    vec[0] += 10; // Fine, before closure even touches it
     {
         let cls_5 = |x| vec[x];
         // vec[0] += 100;  // PROBLEM: changes vec after touched, but before last look
@@ -61,18 +61,16 @@ fn main() {
         println!("cls_5(4) = {}", cls_5(4));
         // println!("cls_5(5) = {}", cls_5(5));
         // // ^ will panic in release and debug due to out of bounds
-        vec[0] += 1000;  // Fine, after closure last looked at it
+        vec[0] += 1000; // Fine, after closure last looked at it
         println!("vec = {:?}", vec);
     }
     println!("----------------------------------------\n");
 
-    // let v_1 = vec![1, 2, 3, 5, 6, 7, 8, 9, 10];
-    // {
-    //     let cls_2 = || {
-    //         let bop = &v_1[out_1..out_2];
-    //         Box(bop)
-    //     };
-    //     println!("v_1 = {:?}", v_1);
-    //     // println!("cls_2() = {}", cls_2());
-    // }
+    let v_1 = vec![1, 2, 3, 5, 6, 7, 8, 9, 10];
+    {
+        let cls_2 = || &v_1[out_1..out_2];
+        println!("v_1 = {:?}", v_1);
+        println!("cls_2() = {:?}", cls_2());
+    }
+    println!("----------------------------------------\n");
 }
