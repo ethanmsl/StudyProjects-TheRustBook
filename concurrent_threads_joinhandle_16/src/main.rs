993a5cd91407c4d8e3fc60e4cbfa86297bf21b01
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-11 12:56:11 -0800

    joining one spawned thread to another and taking a look at what `JoinHandle.join()` actually returns

--- concurrent_threads_joinhandle_16/src/main.rs
+++ concurrent_threads_joinhandle_16/src/main.rs
@@ -1,21 +1,33 @@
 //! concurrent threads -- now with `JoinHandle`s
+//! `JoinHandle`: struct that allows us to use `.join()` to join thread
+//! into another -- amounting, I imagine, to effectively ``await thread_x completion``
 
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(|| {
+    let handle_a = thread::spawn(|| {
         for i in 1..10 {
-            println!("- spawned thread: loop number {}", i);
+            println!("- spawn-thrd--A--: loop number ##{}##", i);
             thread::sleep(Duration::from_millis(1));
         }
     });
 
+    let handle_b = thread::spawn(|| {
+        for i in 1..10 {
+            println!("- spawn-thrd--B--: loop number @ {} @", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+        handle_a.join().unwrap();
+        // ^ joining thread_a to thread_b
+    });
+
     for i in 1..5 {
-            println!("main thread: loop number {}", i);
+        println!("main thread: loop number {}", i);
         thread::sleep(Duration::from_millis(1));
     }
 
-    handle.join().unwrap();
-    // ^ so, I think, `handle.join()` is basically ``await thread_x completion``
+    let _boop = handle_b.join();
+    //  ^               ^ joinging thread_b, which has thread_a joined to it
+    //  ^ a Result is returned - partly just `()`, but also something new...
 }
