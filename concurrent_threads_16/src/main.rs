a5f36f5707b10d88fbf598c4331d0e71c7582be8
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-11 10:25:17 -0800

    nice example of variability of execution times of actions in the two threads

--- concurrent_threads_16/src/main.rs
+++ concurrent_threads_16/src/main.rs
@@ -5,16 +5,20 @@ use std::time::Duration;
 
 fn main() {
     thread::spawn(|| {
-        for i in 1..=20 {
-            println!("hi number {} from the spawned thread!", i);
+        for i in 1..=9 {
+            println!("- spawned thread: loop number {}", i);
             thread::sleep(Duration::from_millis(1));
         }
     });
 
     for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(2));
+        println!("main thread: loop number {}", i);
+        thread::sleep(Duration::from_millis(1));
         //                                  ^ can play with to affect threads
         //                                    from above that occur
+
+    // NOTE: with both sleep-times equal the number of loops printed by the
+    //       spawned thread is variable - as is the precise order of the two
+    //       prints relative to one another
     }
 }
