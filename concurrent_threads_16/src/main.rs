f818a89d45f63a660fd94889315ef68b8b1da7b7
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-11 10:50:03 -0800

    comparing runs without explciit `sleeps` -- note a strange character sometimes appears at the end which may be related to an interrupted `println`(??)

--- concurrent_threads_16/src/main.rs
+++ concurrent_threads_16/src/main.rs
@@ -4,21 +4,47 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
-        for i in 1..=9 {
-            println!("- spawned thread: loop number {}", i);
+    {
+        // Sleep-ensured thread swap opportunities
+        println!("-------------------------------\n");
+        thread::spawn(|| {
+            for i in 1..=9 {
+                println!("- spawned thread: loop number {}", i);
+                thread::sleep(Duration::from_millis(1));
+            }
+        });
+
+        // thread::sleep(Duration::from_millis(1));
+        for i in 1..4 {
+            println!("main thread: loop number {}", i);
             thread::sleep(Duration::from_millis(1));
+            //                                  ^ can play with to affect threads
+            //                                    from above that occur
         }
-    });
-
-    for i in 1..5 {
-        println!("main thread: loop number {}", i);
-        thread::sleep(Duration::from_millis(1));
-        //                                  ^ can play with to affect threads
-        //                                    from above that occur
-
-    // NOTE: with both sleep-times equal the number of loops printed by the
-    //       spawned thread is variable - as is the precise order of the two
-    //       prints relative to one another
+        // NOTE: with both sleep-times equal the number of loops printed by the
+        //       spawned thread is variable - as is the precise order of the two
+        //       prints relative to one another
     }
+
+    {
+        // Many threads with nor forced sleep time
+        println!("-------------------------------\n");
+        thread::spawn(|| {
+            for i in 1..=50 {
+                print!("-sp{}-", i);
+            }
+        });
+
+        // thread::sleep(Duration::from_millis(1));
+        for i in 1..50 {
+            print!("*{}*", i);
+        }
+        // NOTE: with enough repetitions you'll still see the 'spawned' thread
+        //       print as well, *sometimes*
+        // NOTE_2: it also ends with a strange block char of `%` when it ends on
+        //         a spawned char -- is something being interruptd mid-print?
+        println!();
+    }
+
+    println!("-------------------------------\n");
 }
