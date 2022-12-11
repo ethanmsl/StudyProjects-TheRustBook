d133466b939b34388274d0f035d6f0cb7993c426
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-10 18:49:43 -0800

    bidirectional references with strong and `Weak` `Rc`

--- memory_leak_ref_cycles_15/src/main.rs
+++ memory_leak_ref_cycles_15/src/main.rs
@@ -44,25 +44,37 @@ impl Distribution<Muskateer> for Standard {
 }
 ////////////////////////////////////////////////////////////////////////////////
 
+use std::rc::Weak;
+
 #[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 
 fn main() {
     {
         // Using Weak references (which return an Option
+        println!("-----------------------------\n");
 
         let leaf = Rc::new(Node {
             value: 3,
+            parent: RefCell::new(Weak::new()),
             children: RefCell::new(vec![]),
         });
 
+        println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
         let branch = Rc::new(Node {
             value: 5,
+            parent: RefCell::new(Weak::new()),
             children: RefCell::new(vec![Rc::clone(&leaf)]),
         });
+
+        // updating leaf's `parent` field to point to branch
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
     }
 
     {
