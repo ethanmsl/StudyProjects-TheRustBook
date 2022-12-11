bf48d4e1a02eebecceb1e7063f1dfeb949efa91a
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-11 11:04:00 -0800

    letting thread complete via use of `JoinHandle`

--- concurrent_threads_joinhandle_16/src/main.rs
+++ concurrent_threads_joinhandle_16/src/main.rs
@@ -1,3 +1,21 @@
+//! concurrent threads -- now with `JoinHandle`s
+
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("- spawned thread: loop number {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+            println!("main thread: loop number {}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+    // ^ so, I think, `handle.join()` is basically ``await thread_x completion``
 }
