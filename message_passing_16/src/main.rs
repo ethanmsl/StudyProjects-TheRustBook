f733aa7e4fb987015baef376736f9a948c28bc2b
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-12 06:51:36 -0800

    sending soemthing via `tx` was suffiicent for the compiler to infer the type in the let statement above

--- message_passing_16/src/main.rs
+++ message_passing_16/src/main.rs
@@ -1,3 +1,19 @@
+//! Message Passing to trasfer Data Between Threads
+//!
+
+use std::sync::mpsc;
+//              ^ multi-producer, single-consumer
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+
+    // Spawned thread, which communicates via the tx channel defined above
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
+        // ^ NOTE: the last two lines disambiguate the type of the `let (tx, rx)`
+        //         declaration above
+        // ^ also NOTE: that `tx` is *moved* into this closure
+    });
 }
