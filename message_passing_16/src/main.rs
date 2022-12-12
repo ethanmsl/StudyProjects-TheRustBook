f10b6b102c933559a0b5a142a27c32be8fd3c638
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-12 12:36:11 -0800

    example showing `.send()` taking ownership of a value -- commented code would result in compile-time error

--- message_passing_16/src/main.rs
+++ message_passing_16/src/main.rs
@@ -43,13 +43,32 @@ fn main() {
             // no sleep, so fast
         }
 
-        sleep(Duration::from_millis(1200));
-        // ^ can turn on or off to allow the spanwed thread to finish
+        // sleep(Duration::from_millis(1200));
+        // // ^ can turn on or off to allow the spanwed thread to finish
         if let Ok(received) = rx.try_recv() {
-            println!("`rx` got the following message wraped in `Ok()`: {}", received);
+            println!(
+                "`rx` got the following message wraped in `Ok()`: {}",
+                received
+            );
         } else {
             println!("`rx` did not receive an `Ok()` wrapped-result");
         };
         // ^ returns a `Result`, and is non-blocking
     }
+
+    // {
+    //     // Example of trying to use a value after it is send via .send(...)`
+    //     // causes a compile-time error
+    //     // `.send(...)` takes ownership of the value
+    //     let (tx, rx) = mpsc::channel();
+    //
+    //     thread::spawn(move || {
+    //         let val = String::from("hi");
+    //         tx.send(val).unwrap();
+    //         println!("val is {}", val);
+    //     });
+    //
+    //     let received = rx.recv().unwrap();
+    //     println!("`rx` got the following message: {}", received);
+    // }
 }
