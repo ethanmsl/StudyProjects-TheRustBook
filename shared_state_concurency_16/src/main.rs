9e88234414e5f0eb943ccaaa07af3453bea76202
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-12 14:41:20 -0800

    printing mutex before and after lock drop

--- shared_state_concurency_16/src/main.rs
+++ shared_state_concurency_16/src/main.rs
@@ -1,3 +1,26 @@
+//! shared_state_concurrency_16
+//!
+
+use std::sync::Mutex;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    println!("-------------------------------------------------\n");
+    {
+        let m = Mutex::new(5);
+
+        {
+            let mut num = m.lock().expect("\nMutex got poisoned\n");
+            //               ^ *aquire*_lock / lock(_everyone_else_out)
+            *num += 1;
+            println!("m = {:?}", m);
+            //             ^ won't be able to access value of mutex
+            //           as mutex is still 'locked'
+            //           (though it *will* print a value ... "<locked>", which is nice)
+        }
+        // ^ mutex-lock drops due to falling out of scope
+
+        println!("m = {:?}", m);
+    }
+    println!("-------------------------------------------------\n");
 }
