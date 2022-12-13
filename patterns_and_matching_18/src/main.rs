081d8a8500ff503412ebad793e98d30f5ea868cc
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-13 10:03:39 -0800

    mixing `if let`, `else if let`, `else`, and `else if`

--- patterns_and_matching_18/src/main.rs
+++ patterns_and_matching_18/src/main.rs
@@ -1,3 +1,27 @@
+//! Ch 18 -- on patterns and matching
+
 fn main() {
-    println!("Hello, world!");
+    println!("----------------------------------------\n");
+
+    // mixing `if let`, `else if let`, `else if`, and `else`
+    {
+        let favorite_color: Option<&str> = None;
+        let is_tuesday = false;
+        let age: Result<u8, _> = "34".parse();
+
+        if let Some(color) = favorite_color {
+            println!("Using your faovrite color, {color}, as the background");
+        } else if is_tuesday {
+            println!("Tuesday is green day!");
+        } else if let Ok(age) = age {
+            if age > 30 {
+                println!("using purple as the background color");
+            } else {
+                println!("Using orange as the background color");
+            }
+        } else {
+            println!("Using blue as the background color");
+        }
+    }
+    println!("----------------------------------------\n");
 }
