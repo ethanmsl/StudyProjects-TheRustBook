42e76f92690f4533657732eff827412a681f46bc
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-13 04:37:36 -0800

    Some commentary on ptr`<dyn trait>` vs `where T: trait`
    ptr`<dyn trait>` treats the trait like the type that it is, from the
    perspective of the logic-abstraction

--- oop_17/src/lib.rs
+++ oop_17/src/lib.rs
@@ -4,6 +4,64 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
+/// A non-functioning, but organization suggestive library of code
+/// for putting together a gui-library that could have the components it operates on
+/// extended by way of users implementing defined traits on their
+/// own objects/components.
+mod gui {
+
+    /// A trait-type (method-bound type?) that ensures drawability
+    pub trait Draw {
+        /// I suppose I'm based on side-effects and so have no out value (?)
+        fn draw(&self);
+    }
+
+    /// A Vector of `Draw`able types
+    /// (evidently all trait-type specifications require use of a pointer
+    /// (and use the special `dyn` keyword)
+    /// This contrats with the use of 
+    /// ```ignore
+    /// where
+    ///    T: Draw,
+    ///    ...
+    /// ```
+    /// in that it treats, properly (from the logic-abstraction perspective)
+    /// the trait as a type, iteself -- and allows varied, more concrete, types
+    /// to be mixed, single value to single value.
+    pub struct Screen {
+        pub components: Vec<Box<dyn Draw>>,
+        //                     ^ this is **different** than using
+        //                       `where T: Draw`
+        //                       The diffrence is that `T` would have to be
+        //                       a **single** type
+        //                       , whereas <Box<dyn Draw>> can be *multiple*
+        //                       concrete types (at different places in the same struct)
+        //                       it just requires that they all
+        //                       share the same **t-type**
+        //                       [Sidenote: logically, (re: the logic-abstraction level)
+        //                        Traits are just a "type",
+        //                        Rust does not seem to treat them this way and, 
+        //                        implementation wise there *may* be good reasons for
+        //                        this -- however traits are just nodes and
+        //                        functions/methods are just edges / morphisms
+        //                        in a general network / pre-category]
+        //
+    }
+
+    /// Do we get docs for an `impl` block???  
+    /// *Hi, if so!*
+    impl Screen {
+        /// Draw all the members of the `components` field of a `Screen` object
+        pub fn run(&self) {
+            for component in self.components.iter() {
+                component.draw();
+            }
+        }
+    }
+
+
+
+}
 ///////////////////////////////////////////////////////////////////////
 
 /// Does this create documentation for the module `avg_cache`?  
