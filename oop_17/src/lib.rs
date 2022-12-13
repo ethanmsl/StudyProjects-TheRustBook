a7692fe0e076bde0add9bfff4946371f5c17fe1b
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-13 02:49:12 -0800

    Nice struct and mix of public and private methods to ensure certain behavior when externally interfaced with.

--- oop_17/src/lib.rs
+++ oop_17/src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! Object Oriented Like Characteristics of Rust Ch. 17
+//!
+
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+/// look at me, I'm documentation
+/// I store a vector and an average of it's terms
+/// *de facto* caching the average
+pub struct AveragedCollection {
+    list: Vec<i32>,
+    average: f64,
+}
+
+impl AveragedCollection {
+    /// ensures that the average field is always updated
+    /// when vector elements are added
+    pub fn add(&mut self, value: i32) {
+        self.list.push(value);
+        self.update_average();
+    }
+
+    pub fn remove(&mut self) -> Option<i32> {
+        let result = self.list.pop();
+        match result {
+            Some(value) => {
+                self.update_average();
+                Some(value)
+            }
+            None => None,
+        }
+    }
+
+    pub fn average(&self) -> f64 {
+        self.average
+    }
+
+    fn update_average(&mut self) {
+        let total: i32 = self.list.iter().sum();
+        self.average = total as f64 / self.list.len() as f64;
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
