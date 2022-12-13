4dfba8143d9a94d7ef27a31431abbafe383b78e6
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-13 13:53:30 -0800

    more match guard code :)

--- patterns_and_matching_18/src/main.rs
+++ patterns_and_matching_18/src/main.rs
@@ -447,6 +447,29 @@ fn main() {
         //         None => (),
         //     }
         // }
+
+        let x = Some(5);
+        let y = 10;
+
+        // with match guard
+        match x {
+            Some(50) => println!("Got 50"),
+            Some(n) if n == y => println!("Matched, n = {}", n),
+            _ => println!("Default case, x = {:?}", x),
+        }
+
+        // without match guards
+        match x {
+            Some(50) => println!("Got 50"),
+            Some(n) => {
+                if n == y {
+                    println!("Matched, n = {}", n)
+                }
+            }
+            _ => println!("Default case, x = {:?}", x),
+        }
+
+        println!("at the end: x = {:?}, y = {}", x, y);
     }
     println!("----------------------------------------\n");
 }
