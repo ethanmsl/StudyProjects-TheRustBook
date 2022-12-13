8d987703bc68dfc9e516e3e186e8983e5e38595e
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-13 07:25:40 -0800

    state change early

--- oop_state_pattern_17/src/blog_oop_trad.rs
+++ oop_state_pattern_17/src/blog_oop_trad.rs
@@ -27,10 +27,34 @@ impl Post {
     pub fn content(&self) -> &str {
         ""
     }
+
+    /// request review of Post
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
 }
 
-trait State {}
+trait State {
+    /// changes state from one thing to another that implements the State trait
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+}
 
 struct Draft {}
 
-impl State for Draft {}
+impl State for Draft {
+    /// changes state from `Draft` ~~> `PendingReview`
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    /// identity function (doesn't actually change state)
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
