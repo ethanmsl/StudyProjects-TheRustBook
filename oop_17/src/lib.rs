3b9ae9aa3d0f9f325baa0bfa4bc6cc6a083081d0
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-13 04:14:12 -0800

    made a local module of averagedcollection related code

--- oop_17/src/lib.rs
+++ oop_17/src/lib.rs
@@ -4,40 +4,44 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
 
-/// look at me, I'm documentation
-/// I store a vector and an average of it's terms
-/// *de facto* caching the average
-pub struct AveragedCollection {
-    list: Vec<i32>,
-    average: f64,
-}
-
-impl AveragedCollection {
-    /// ensures that the average field is always updated
-    /// when vector elements are added
-    pub fn add(&mut self, value: i32) {
-        self.list.push(value);
-        self.update_average();
+/// Does this create documentation for the module `avg_cache`?
+/// **Hi***!*
+mod avg_cache {
+    /// look at me, I'm documentation
+    /// I store a vector and an average of it's terms
+    /// *de facto* caching the average
+    pub struct AveragedCollection {
+        list: Vec<i32>,
+        average: f64,
     }
 
-    pub fn remove(&mut self) -> Option<i32> {
-        let result = self.list.pop();
-        match result {
-            Some(value) => {
-                self.update_average();
-                Some(value)
-            }
-            None => None,
+    impl AveragedCollection {
+        /// ensures that the average field is always updated
+        /// when vector elements are added
+        pub fn add(&mut self, value: i32) {
+            self.list.push(value);
+            self.update_average();
         }
-    }
 
-    pub fn average(&self) -> f64 {
-        self.average
-    }
+        pub fn remove(&mut self) -> Option<i32> {
+            let result = self.list.pop();
+            match result {
+                Some(value) => {
+                    self.update_average();
+                    Some(value)
+                }
+                None => None,
+            }
+        }
 
-    fn update_average(&mut self) {
-        let total: i32 = self.list.iter().sum();
-        self.average = total as f64 / self.list.len() as f64;
+        pub fn average(&self) -> f64 {
+            self.average
+        }
+
+        fn update_average(&mut self) {
+            let total: i32 = self.list.iter().sum();
+            self.average = total as f64 / self.list.len() as f64;
+        }
     }
 }
 
