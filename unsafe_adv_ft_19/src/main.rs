27845a9fbe381b45bded29e5ffdf9c6de261c9cb
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-14 03:22:53 -0800

    creating and safely and unsafely using raw pointers

--- unsafe_adv_ft_19/src/main.rs
+++ unsafe_adv_ft_19/src/main.rs
@@ -1,3 +1,33 @@
+//! Unsafe Rust -- Ch 19  Advanced Features
+
 fn main() {
-    println!("Hello, world!");
+    println!("----------------------------------------\n");
+
+    // Creating Raw Pointers and Safe & Unsafe use
+    {
+        let mut num = 5;
+
+        // casting *ref* to `num` as raw pointer*s*
+        // NOTE: no `unsafe` keyword needed to *create* the raw pointers
+        let r1 = &num as *const i32;
+        let r2 = &mut num as *mut i32;
+
+        println!("safe: r1 is: {:?}", r1);
+        println!("safe: r2 is: {:?}", r2);
+        unsafe {
+            println!("unsafe: r1 is: {:?}", r1);
+            println!("unsafe: r2 is: {:?}", r2);
+            println!("unsafe: *r1 is: {:?}", *r1);
+            println!("unsafe: *r2 is: {:?}", *r2);
+        }
+
+        let address = 0x012345usize;
+        let r = address as *const i32;
+
+        println!("safe: r is: {:?}", r);
+        unsafe {
+            // println!("unsafe: *r is: {:?}", *r);
+            // // ^ lol, resulted in `segmentation fault`
+        }
+    }
 }
