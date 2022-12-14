489c1b01cd2411c08c7716de7b13d969ad4ae58e
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-14 04:45:57 -0800

    "safe abstraction" seems to mean "didn't label the function unsafe" ...

--- unsafe_adv_ft_19/src/main.rs
+++ unsafe_adv_ft_19/src/main.rs
@@ -43,5 +43,60 @@ fn main() {
         unsafe {
             dangerous();
         }
+
+        // example of existing unsafe function safely wrapped
+        // `.split_at_mut(_)`
+        {
+            let mut v = vec![1, 2, 3, 4, 5, 6];
+            println!("v: {:?}, len(v): {}", v, v.len());
+            let r = &mut v[..];
+            let (a, b) = r.split_at_mut(3);
+            //                          ^ specifies place *between* elements
+            //                            re: 0-indexing meaning
+            assert_eq!(a, &mut [1, 2, 3]);
+            assert_eq!(b, &mut [4, 5, 6]);
+        }
+
+        // seeing if our `split_at_mut_bespoke` works
+        // huh ... it does
+        // so this "safe abstraction" is just ... not explicitely
+        // labelling a function as unsafe and instead putting an unsafe block
+        // inside it...  ...'k
+        {
+            do_a_thing();
+            let mut v = vec![1, 2, 3, 4, 5, 6];
+            println!("v: {:?}, len(v): {}", v, v.len());
+            let r = &mut v[..];
+            let (a, b) = split_at_mut_bespoke(r, 3);
+            //                          ^ specifies place *between* elements
+            //                            re: 0-indexing meaning
+            assert_eq!(a, &mut [1, 2, 3]);
+            assert_eq!(b, &mut [4, 5, 6]);
+            i_did_a_thing();
+        }
+    }
+}
+
+// remake of `split_at_mut`
+use std::slice;
+
+fn do_a_thing() -> () {
+    println!("I'm doing a thing!");
+}
+fn i_did_a_thing() -> () {
+    println!("I did a thing!");
+}
+
+fn split_at_mut_bespoke(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+    let len = values.len();
+    let ptr = values.as_mut_ptr();
+
+    assert!(mid <= len);
+
+    unsafe {
+        (
+            slice::from_raw_parts_mut(ptr, mid),
+            slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+        )
     }
 }
