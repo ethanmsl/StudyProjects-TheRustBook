ba227d8ef0bd9c9b161bcb743c3fbdb851b82347
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-18 12:28:43 -0800

    findings: two separate issues: (1) `RustFmt` & `RustFmtRange` aren't meant to be part of the rust-tools plugin and don't work (2) `let-else` formatting by `cargo fmt` appears to be broken (and related issues are reported)

--- let_else_xx/src/main.rs
+++ let_else_xx/src/main.rs
@@ -1,21 +1,86 @@
 //! just playing with syntax, particularly `let-else` and variants
+use log;
 
 fn main() {
     // write a let statement declaring a result type
+    println!("---------------------------------\n");
 
-    let res: Option<i32> = Some(111);
+    let opt: Option<i32> = Some(111);
+    dbg!(opt);
 
-    let Some(x) = res else {
-    return;
-    };
-
-    dbg!(x);
-
-    let res: Option<i32> = None;
-
-    let Some(x) = res else {
-    println!("\nno value\n");
-    return;
+    let Some(x) = opt else {
+        println!("res is None");
+                    println!("returning...");
+return;
     };
     dbg!(x);
+    println!("---------------------------------\n");
+
+    // let opt: Option<i32> = None;
+    dbg!(opt);
+
+    let Some(x) = opt else {
+println!("\nno value\n");
+println!("value of 'opt' is {:?}", opt);
+                        println!("returning...");
+        return;
+    };
+    dbg!(x);
+
+    println!("---------------------------------\n");
+
+    let res: Result<i32, &str> = Ok(222);
+    dbg!(res);
+
+    let Ok(x) = res else {
+        println!("res is error {}", res.unwrap());
+        println!("returning...");
+        return;
+    };
+    dbg!(x);
+    println!("---------------------------------\n");
+
+    // let res: Result<i32, &str> = Err("error");
+    dbg!(res);
+
+    let Ok(x) = res else {
+        println!("res is error {:?}", res);
+        println!("returning...");
+        return;
+    };
+    dbg!(x);
+    println!("---------------------------------\n");
+
+    // let res: Result<i32, &str> = Err("error");
+    dbg!(res);
+
+    let Ok(x) = res else {
+        let err = res.unwrap_err();
+        println!("this should print: {}", err);
+        println!("returning...");
+        return;
+    };
+    dbg!(x);
+    println!("---------------------------------\n");
+
+    // let res: Result<i32, &str> = Err("error");
+    dbg!(res);
+
+    let Ok(x) = res else {
+        res.expect("\n--printed from expect method--\n");
+        println!("this shouldn't print");
+        println!("returning...");
+        return;
+    };
+    dbg!(x);
+    println!("---------------------------------\n");
+
+    let res: Result<i32, &str> = Err("error_for_you");
+    dbg!(res);
+
+    let Ok(val) = res else {
+        let err = res.unwrap_err();
+        eprintln!("Failed to get value: {err}");
+        return;
+    };
 }
