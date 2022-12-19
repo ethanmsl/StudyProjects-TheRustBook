676fa75ca213ab79f88350ef9c99fb7c7b300b9f
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-18 16:13:46 -0800

    implementing a version of the Iterator trait

--- traits_adv_ft_19/src/main.rs
+++ traits_adv_ft_19/src/main.rs
@@ -1,3 +1,50 @@
+//! Advanced Traits -- Ch 19 Advanced Features
+
+/// Same as `Iterator`; here to look at syntax
+pub trait IteratorBespoke {
+    type Item;
+
+    fn next(&mut self) -> Option<Self::Item>;
+}
+
+/// a one field struct
+pub struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    /// create a new, zero-valued `Counter`
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+/// I don't think this gets added to docs (the comment, that is)
+/// This is a *twisted*, almost malicious implementation of `next` (copilot suggested)
+/// Interesting to consider whether it would properly be acceptable...
+/// I suppose... so long as the only assumption is that things that use it have
+/// the contracts agreed upon ... then yes
+impl IteratorBespoke for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        // l-o-f'ing-l
+        if self.count < 6 {
+            None
+        } else {
+            Some(self.count)
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cntr = Counter::new();
+    for _ in 0..12 {
+        match cntr.next() {
+            Some(x) => println!("cntr: {}", x),
+            None => println!("cntr: None, too small"),
+        }
+    }
 }
