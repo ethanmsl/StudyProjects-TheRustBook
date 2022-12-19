15533fcb4ba722e8dbeb7404184bc6e04c63f823
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-19 07:59:22 -0800

    a function taking a function, with the input function signature as part of the type declaration

--- functions_and_closures_adv_19/src/main.rs
+++ functions_and_closures_adv_19/src/main.rs
@@ -1,3 +1,15 @@
+//! Advanced Functions and Closures -- Advanced Features Ch 19
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
+
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f( f(arg) )
+}
+
 fn main() {
-    println!("Hello, world!");
+    let answer = do_twice(add_one, 5);
+
+    println!("The answer is: {}", answer);
 }
