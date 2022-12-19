1036accb0414fb10b839832a1c3846fe6f6f8ead
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-18 16:55:06 -0800

    default params and traits and getting overloading via param type specification

--- traits_adv_ft_19/src/main.rs
+++ traits_adv_ft_19/src/main.rs
@@ -31,7 +31,7 @@ impl IteratorBespoke for Counter {
         //                         ^ this is a bit confusing tome
         //          as it's really just whatever type 'item' is which isn't related to,
         //          in this case, IteratorBespoke or Counter.
-        //          I *think* the best way to think about it is 
+        //          I *think* the best way to think about it is
         //          as `Item` belongs to the trait-type (`IteratorBespoke`)
         //          , the whole thing being replaced by the concrete type supplied
         self.count += 1;
@@ -46,11 +46,64 @@ impl IteratorBespoke for Counter {
 }
 
 fn main() {
-    let mut cntr = Counter::new();
-    for _ in 0..12 {
-        match cntr.next() {
-            Some(x) => println!("cntr: {}", x),
-            None => println!("cntr: None, too small"),
+    println!("---------------------------------------------\n");
+    // `IteratorBespoke` code
+    {
+        let mut cntr = Counter::new();
+        for _ in 0..12 {
+            match cntr.next() {
+                Some(x) => println!("cntr: {}", x),
+                None => println!("cntr: None, too small"),
+            }
         }
+    }
+    println!("---------------------------------------------\n");
+
+    // Default Type Params & Overloading
+    {
+        use std::ops::Add;
+
+        #[derive(Debug, Copy, Clone, PartialEq)]
+        struct Point {
+            x: i32,
+            y: i32,
+        }
+
+        impl Point {
+            fn new(x: i32, y: i32) -> Self {
+                Point { x, y }
+            }
+        }
+
+        impl Add for Point {
+            type Output = Point;
+
+            fn add(self, other: Point) -> Point {
+                Point::new(self.x + other.x, self.y + other.y)
+            }
+        }
+        let p1 = Point::new(1, 0);
+        let p2 = Point::new(2, 3);
+        let p3 = p1.add(p2);
+        dbg!(p3);
+        let p4 = p1 + p3;
+        dbg!(p4);
+
+        impl Add<i32> for Point {
+            type Output = Point;
+
+            fn add(self, other: i32) -> Point {
+                Point::new(self.x + other, self.y + other)
+            }
+        }
+
+        let p1 = Point::new(1, 0);
+        let p3 = p1.add(10);
+        dbg!(p3);
+        let p4 = p3 + 5;
+        dbg!(p4);
+        // let p5 = 5 + p3;
+        // dbg!(p5);
+        // // no impl for i32+Point
     }
 }
