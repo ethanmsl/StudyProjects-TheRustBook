b699a389554681c35b42a3761c5059a80d771b8b
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-19 08:04:49 -0800

    type aliases can be used used for function signatures, but `where` seems to be for traits only

--- functions_and_closures_adv_19/src/main.rs
+++ functions_and_closures_adv_19/src/main.rs
@@ -5,11 +5,30 @@ fn add_one(x: i32) -> i32 {
 }
 
 fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
-    f( f(arg) )
+    f(f(arg))
+}
+
+// /// not valid
+// /// the `where` syntax seems to be for traits only
+// fn do_thrice(f: F, arg: i32) -> i32
+// where
+//     F = Fn(i32) -> i32,
+// {
+//     f(f(f(arg)))
+// }
+
+type I32x2 = fn(i32) -> i32;
+
+fn do_thrice(f: I32x2, arg: i32) -> i32
+{
+    f(f(f(arg)))
 }
 
 fn main() {
     let answer = do_twice(add_one, 5);
-
     println!("The answer is: {}", answer);
+
+    let answer = do_thrice(add_one, 5);
+    println!("The answer is: {}", answer);
+
 }
