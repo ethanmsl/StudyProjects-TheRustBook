b5f7d569126a587def40b6e0311fdfc875afe735
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-22 10:04:13 -0800

    seperators appear to have special rules for their use that makes common uses cases easy (but do not allow trailing seperators, some what surprisingly)
    so the `*` applies to the whole `(...),` pattern abutting it and the `,`
    element is given special treatment as a non-trailing patterned
    seperator.  --  At least that's what I *think* is going on.

--- macros_adv_ft_19/src/main.rs
+++ macros_adv_ft_19/src/main.rs
@@ -1,13 +1,41 @@
-
 use macros_adv_ft_19 as lib;
 
 fn main() {
-    let v = lib::vec_bespoke![1, 2, 3];
-    println!("v = {:?}", v);
+    // using our "declarative" (pattern match and replace) macro
+    {
+        let v = lib::vec_bespoke![1, 2, 3];
+        println!("v = {:?}", v);
+
+        // let v = lib::vec_bespoke![1, 2,,, 3,];
+        // //                             ^ not sure why that's not allowed since we used
+        // //                               `,*` in the macro...
+        // //                               ^ ANSWER (maybe): because "separator"s are
+        // //                                 treated specially.
+        // //                            The `*` applies to the whole `(...),` next to the `*`
+        // //                            and the `,` just means ~"and allow comma as a separtor"
+        // //                            and it *seems* to have special rules that allow classic
+        // //                            sepparator usage (with*out* trailing separators)
+        // println!("v = {:?}", v);
+
+        // let v = lib::vec_bespoke![1, 2, 3,];
+        // //                               ^ the default macro sepperator rules **don't** allow for
+        // //                                 trailing separators
+        // println!("v = {:?}", v);
+    }
 
     // let v = lib::vec_bespoke![1, 2,,, 3,];
     // //                             ^ not sure why that's not allowed since we used
     // //                               `,*` in the macro...
+    // //                               ^ ANSWER (maybe): because "separator"s are
+    // //                                 treated specially.
+    // //                            The `*` applies to the whole `(...),` next to the `*`
+    // //                            and the `,` just means ~"and allow comma as a separtor"
+    // //                            and it *seems* to have special rules that allow classic
+    // //                            sepparator usage (with*out* trailing separators)
     // println!("v = {:?}", v);
 
+    // let v = lib::vec_bespoke![1, 2, 3,];
+    // //                               ^ the default macro sepperator rules **don't** allow for 
+    // //                                 trailing separators
+    // println!("v = {:?}", v);
 }
