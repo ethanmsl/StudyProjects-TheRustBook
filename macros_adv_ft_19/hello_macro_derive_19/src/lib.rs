3ed7164b5459b30b81555e96e354e98835cf0993
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-23 06:20:11 -0800

    trait-deriving-macro using dedicated procedural-macro crate

--- macros_adv_ft_19/hello_macro_derive_19/src/lib.rs
+++ macros_adv_ft_19/hello_macro_derive_19/src/lib.rs
@@ -1,14 +1,61 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use proc_macro::TokenStream;  // built-in; prob. required the `proc-macro` flag in toml
+use quote::quote;  // (rust AST ~~> tokenstream)
+use syn;  // Parsing library (rust tokenstream ~~> AST)
+
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    // Construct a represenation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = syn::parse(input).unwrap();
+
+    //Build the trait implementation
+    impl_hello_macro(&ast)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+// # Using the #[derive(...)] on an example struct
+// ## an example AST structure built from the tokenstream
+//
+// #[derive(HelloMacro)]
+// struct Pancakes;
+//
+// ~~~>
+//
+// DeriveInput {
+//     // --snip--
+//
+//     ident: Ident {
+//         ident: "Pancakes",
+//         span: #0 bytes(95..103)
+//     },
+//     data: Struct(
+//         DataStruct {
+//             struct_token: Struct,
+//             fields: Unit,
+//             semi_token: Some(
+//                 Semi
+//             )
+//         }
+//     )
+// }
+
+
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}", stringify!(#name));
+                //                                         ^ stringify!() is a macro that
+                //                                           converts an expression into a
+                //                                           string literal
+                //                          Question: is this comment technically
+                //                          getting built as part of the derive macro?
+                //                          And would there be a way for me to see that
+                //                          since for (at least) many build set-ups the
+                //                          compiler would strip out the comments...?
+            }
+        }
+    };
+    gen.into()
 }
--- macros_adv_ft_19/src/main.rs
+++ macros_adv_ft_19/src/main.rs
@@ -1,6 +1,16 @@
 use macros_adv_ft_19 as lib;
+use lib::HelloMacro;  // where we defined trait (which, alas, has "macro" in its name)
+use hello_macro_derive_19::HelloMacro;  // where we defined macro trait deriver
+//                          ^ that I'm calling this rather than the
+//                            `hello_macro_derive` surprises me...
+//                            I suppose it's some nominal convenience-oriented peculiarity
+
+#[derive(HelloMacro)]
+/// unit struct
+struct Pancakes;
 
 fn main() {
+    println!("---------------------------------------------\n");
     // using our "declarative" (pattern match and replace) macro
     {
         let v = lib::vec_bespoke![1, 2, 3];
@@ -22,9 +32,16 @@ fn main() {
         // //                                 trailing separators
         // println!("v = {:?}", v);
     }
+    println!("---------------------------------------------\n");
 
-    // using our trait implementor macro
+    // using our "HelloMacro" trait implementor macro
+    //             ^ note that's the name of the *trait*, not the macro
+    //               (questionable naming scheme, but I'm not going to go back
+    //                and repair it all right now)
     {
-
+        // let immapancake = Pancakes;
+        // immapancake.hello_macro();
+        Pancakes::hello_macro();
+        // ^ the function is for the struct itself, rather than its instances
     }
 }
