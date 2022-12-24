5622335e8225309b1afc6399021785103cde9f40
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-23 17:01:42 -0800

    feat: capturing the specific http-request data

--- hello_20/src/main.rs
+++ hello_20/src/main.rs
@@ -5,8 +5,12 @@
 //! ^, ^^ both are request-response protocols
 //! TCP is a lower-level protocol which HTTP is (usually) sent atop of
 //!
+//! Run program then use a browser (for example) aimed at "127.0.0.1:7878"
 
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -19,7 +23,7 @@ fn main() {
     //                  Note: IPv6 uses `::1` as localhost
 
     for stream in listener.incoming() {
-        //                 ^ returns an iterator over connection attempts received on this 
+        //                 ^ returns an iterator over connection attempts received on this
         //                   "listener"
         //                 WHERE "connection" represents the whole request&respond
         //                 process -- and a "connection" is "closed" when we respond
@@ -28,5 +32,33 @@ fn main() {
         let stream = stream.unwrap();
 
         println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    //                  ^ wraps the stream data, while doing "infrequent" chunk reads
+    //                    so that we can make small 'frequent' calls to it
+    //                    without the innefficiency of the same number of system calls
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    //   ^ we take all the elements up until the nominal end of the request as denoted
+    //     by an empty line (consecutive endline chars in the raw stream)
+    //     , but it doesn't seem like we're closing the connection at any point
+    //     (perhaps that's done by dint of the function taking ownership of the
+    //     connection and then destroying it's when it de-scopes (?) )
+    //                       ^ the stream isn't precisely a memory element (I assume)
+    //                         so that's not strictly needed as far as I can tell
+
+    // ^ I assume the above is only good practice because we make an assumption that
+    //   the request is finite and of a reasonable length
+    //   (/ perhaps that's something that a more robust system would need to account for
+    //    or perhaps it's something that the HTTP protocol itself guarantees (?) )
+
+    println!("Request: {:#?}", http_request);
 }
