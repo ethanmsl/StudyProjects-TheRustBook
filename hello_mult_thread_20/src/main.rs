b5c9c1e14ca101875fc7e7f638c5a5ab7b1b018b
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-25 13:00:01 -0800

    style-chore: cleaned up a lot of the heavy comments from the first iteration and implemented the simpler if-else branch & value format

--- hello_mult_thread_20/src/main.rs
+++ hello_mult_thread_20/src/main.rs
@@ -39,56 +39,17 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    //                  ^ wraps the stream data, while doing "infrequent" chunk reads
-    //                    so that we can make small 'frequent' calls to it
-    //                    without the innefficiency of the same number of system calls
-
-    // let http_request: Vec<_> = buf_reader
-    //     .lines()
-    //     .map(|result| result.unwrap())
-    //     .take_while(|line| !line.is_empty())
-    //     .collect();
-
-    //   ^ we take all the elements up until the nominal end of the request as denoted
-    //     by an empty line (consecutive endline chars in the raw stream)
-    //     , but it doesn't seem like we're closing the connection at any point
-    //     (perhaps that's done by dint of the function taking ownership of the
-    //     connection and then destroying it's when it de-scopes (?) )
-    //                       ^ the stream isn't precisely a memory element (I assume)
-    //                         so that's not strictly needed as far as I can tell
-
-    // ^ I assume the above is only good practice because we make an assumption that
-    //   the request is finite and of a reasonable length
-    //   (/ perhaps that's something that a more robust system would need to account for
-    //    or perhaps it's something that the HTTP protocol itself guarantees (?) )
-
-    // println!("Request: {:#?}", http_request);
-    // // ^ console output to let us see details of request
-
-    // let response = "HTTP/1.1 200 OK\r\n\r\n";
-    // //  ^ custom & complete HTTP response; success, no more details
-
     let request_line = buf_reader.lines().next().unwrap().unwrap();
-    // Notable that we have two unwraps in a row -- related to the
-    // delayed/as_needed/"lazy" style of the iterator I assume
-    // Specifically: `lines()` returns an iterator, which *will* return a Result<>
-    // and then `.next()` returns a definite object, which is *also* a Result<>
 
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let filename = "hello.html";
-        (status_line, filename)
+        ("HTTP/1.1 200 OK", "hello.html")
     } else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let filename = "404.html";
-        (status_line, filename)
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
-        let contents = fs::read_to_string(filename).unwrap();
-        let length = contents.len();
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
-        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-        stream.write_all(response.as_bytes()).unwrap();
-        //                                     ^ unwraps a `Result<()> -- which is just a
-        //                                       way of proviging an Ok|x|Error code.`
+    stream.write_all(response.as_bytes()).unwrap();
 }
