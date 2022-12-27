ea7ecf589c30a92b2b85d40229e9e98894549d46
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2022-12-26 16:55:46 -0800

    feat: duplicated code from `hello_mult_thread_20` and simplified slightly

--- hello_and_graceful_goodbyes_20/src/lib.rs
+++ hello_and_graceful_goodbyes_20/src/lib.rs
@@ -1,14 +1,84 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! lib.rs file
+//! currently contains our `ThreadPool` implementation
+
+use std::{
+    fmt,
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// Errors that can occur when creating a new `ThreadPool`.
+/// (currently only one kind: mis-initialization with zero threads)
+#[derive(Debug, Clone)]
+pub enum PoolCreationError {
+    InvalidThreadCount,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Failed to create ThreadPool")
+    }
+}
+
+impl ThreadPool {
+    /// Build a new ThreadPool.
+    /// implicitly errorable creation "build" (vs implicitly guaranteed creation "new")
+    /// returns a specific `PoolCreationError` if called with invalid argument
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// NOTE: `new` should be guaranteed to work, so this is
+    ///       poor use of convention; but I'm running with our book here
+    ///
+    /// # Panics
+    ///
+    pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size == 0 {
+            return Err(PoolCreationError::InvalidThreadCount);
+        }
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        Ok(ThreadPool { workers, sender })
+    }
+
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("--Worker {} got a job; executing.--", id);
+
+            job();
+        });
+
+        Worker { id, thread }
     }
 }
---
+++ hello_and_graceful_goodbyes_20/src/main.rs
@@ -0,0 +1,74 @@
+//! Rust Book: Ch 20 - Final Project: Building a Multithreaded Web Server
+//!
+//! HTTP - HyperText Transfer Protocol, specific server requests
+//! TCP - Transmission Control Protocol, routing of data from one server to another
+//! ^, ^^ both are request-response protocols
+//! TCP is a lower-level protocol which HTTP is (usually) sent atop of
+//!
+//! Run program then use a browser (for example) aimed at "127.0.0.1:7878"
+
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
+
+use hello_and_graceful_goodbyes_20::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    //                                _________
+    //                                   |      ____
+    //                                   |       |
+    //                                   |       port
+    //                        IPv4 "loopback" or "localhost" address
+    //                            (generic across systems)
+    //                  Note: IPv6 uses `::1` as localhost
+
+    let pool = ThreadPool::build(4).expect("Failed to build ThreadPool");
+
+    for stream in listener.incoming() {
+        //                 ^ returns an iterator over connection attempts received on this
+        //                   "listener"
+        //                 WHERE "connection" represents the whole request&respond
+        //                 process -- and a "connection" is "closed" when we respond
+        //                 so basically we're iterating over un-responded to requests
+        //
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            println!("Connection established!");
+            handle_connection(stream);
+        });
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = match &request_line[..] {
+        //                              ^ NOTE: `match` does not auto ref & deref
+        "GET / HTTP/1.1" => {
+            println!("'Get /' request received");
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        "GET /sleep HTTP/1.1" => {
+            println!("'Get /sleep' request received");
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => {
+            println!("unrecognized request received");
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        }
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
